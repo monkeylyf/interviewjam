b3fc1b3ff6c778de7a327bb1cd61d77090aae156
Yifeng Liu <yifengliu@dhcp-173-37-1-231.cisco.com>
2012-10-21 14:50:37 -0700

    test 49 & 50 added.

--- recursion/test_50.java
+++ recursion/test_50.java
@@ -7,9 +7,30 @@ Design an algorithm to get all possible paths for the robot*/
 
 import java.util.*;
 
+
+
 class test_50 {
 	public static void main(String[] args) {
-		ArrayList<Point> cur_path = new ArrayList<Point>();
+		System.out.println("Does this work?");
+		Grid g = new Grid();
+	}
+}
+
+class Grid {
+	ArrayList<Point> cur_path = new ArrayList<Point>();
+
+	public static boolean getPaths(int x, int y) {
+		Point p = new Point(x, y);
+		cur_path.add(p);
+		if (x == 0 && y == 0) return true;
+		boolean success = false;
+		if (x >= 1 && is_free(x - 1, y)) {success = getPaths(x - 1, y);}
+		if (!success && y >= 1 && is_free(x, y - 1)) {success = getPaths(x, y - 1);}
+		if (!success) {cur_path.remove(p);}
+	}
+
+	public static is_free(int x, int y) {
+		
 	}
 }
 
