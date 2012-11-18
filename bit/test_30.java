d41bb93fa29df1c35a32d46ad2b04c1278402660
monkeylyf <laituan1986@gmail.com>
2012-11-17 23:17:03 -0800

    add the solution to the followup question of test_30

--- bit/test_30.java
+++ bit/test_30.java
@@ -1,12 +1,22 @@
-/*Given an integer, print the next smallest and next largest number that have the same
-number of 1 bits in their binary representation*/
+/*Given an integer, print the next smallest and next largest number that have
+the same number of 1 bits in their binary representation.
+FOLLOWUP
+1. Given an integer, print the next smallest and next largest number that have the
+same number of 0, 1, 2, ... 9, in their deximal representation.
+2. Permutate a string
+*/
+
+import java.util.*;
 
 class test_30 {
 	public static void main(String[] args) {
 		int input = 121;
 		nextLargest(input);
 		nextSmallest(input);
-	
+		followup(123456789);
+		followup(123654321);
+		followup(987654321);
+		permutate(12345);
 	}
 	public static void nextLargest(int input) {
 		String str = Integer.toBinaryString(input);
@@ -53,5 +63,40 @@ class test_30 {
 		}
 		System.out.println(str);
 		System.out.println(new String(arr));
+	}
+	public static int followup(int input) {
+		String str = Integer.toString(input);
+		char[] arr = str.toCharArray();
+		int tail = arr.length - 1;
+		int head = 0;
+		for (head = tail; head > 0; --head) {
+			if (arr[head] > arr[head - 1]) {
+				for (int j = tail; j >= head; --j) {
+					if (arr[j] > arr[head - 1]) {
+						char swap = arr[head - 1];
+						arr[head - 1] = arr[j];
+						arr[j] = swap;
+						break;
+					}
+				}
+				break;
+			}
+		}
+		if (head != 0) {
+			while (head < tail) {
+				char swap = arr[head];
+				arr[head] = arr[tail];
+				arr[tail] = swap;
+				++head;
+				--tail;
+			}
+		}
+		return Integer.parseInt(new String(arr));
+	}
+	public static void permutate(int input) {
+		int result = followup(input);
+		System.out.println(input);
+		if (result == input) return;
+		permutate(result);
 	}
 }
