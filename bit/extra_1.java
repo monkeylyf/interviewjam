1b1e440fa1989a99b7d6db1e13f49535375bb0fb
monkeylyf <laituan1986@gmail.com>
2012-11-29 20:27:48 -0800

    Done with eight queens problems...finally. added a extra test under bit dir

---
+++ bit/extra_1.java
@@ -0,0 +1,18 @@
+/*
+1. How many different binary numbers having a number of 1 and b number of 0?
+2. Put all the possible numbers in a sorted list. Given a index, could you
+calculate what that number is?
+3. Given the number in this list, could you calculate its index?
+
+Solution for question 1:
+C
+
+*/
+
+
+
+class extra_1 {
+	public static void main(String[] args) {
+	
+	}
+}
