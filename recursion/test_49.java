3045c440f981b917c287f4a74827ee0887bbe5bd
monkeylyf <laituan1986@gmail.com>
2012-12-07 19:18:42 -0800

    update test_49 with a O(logn) time complexity solution to get the nth element of a Fibbonaci; update test_56 the eight queens problem

--- recursion/test_49.java
+++ recursion/test_49.java
@@ -2,11 +2,46 @@
 
 class test_49 {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; ++i) System.out.println(Fibonacci(i));	
+		for (int i = 0; i < 10; ++i) System.out.println(Fibonacci(i));
+		for (int i = 0; i < 10; ++i) System.out.println(Fibo(i));
 	}
+	// O(n) time complexy.
 	public static int Fibonacci(int n) {
 		if (n <= 0) return 0;
 		if (n == 1) return 1;
 		return Fibonacci(n - 1) + Fibonacci(n - 2);
+	}
+	// O(log n) time complexity.
+	// a(n) = p * a(n - 1) + q * a(n - 2)
+	// Equation: ( a(n)  ) = ( p^2 + q    p) * ( a(n-2) )
+	//             a(n-1)      p          q      a(n-3)
+	// In this case of Fibonacci, q = p = 1
+	// Then we have:
+	// ( a(2n+1)) = (2 1)^n * ( a(1) )
+	//   a(2n)       1 1        a(0)
+	// a(1) = a(0) = 1
+	// Finally we have:
+	// ( a(2n+1)) = (2 1)^n * (1)
+	//   a(2n)       1 1	   1
+	// We need to calculate (2 1) ^ n in O(log n)time cmoplexity  
+	//			 1 1	
+	public static int Fibo(int n) {
+		if (n == 0 || n == 1) return 1;
+		int k = n / 2;
+		int[] val = new int[] {1, 0, 0, 1};  
+		int[] m = new int[] {2, 1, 1, 1};
+		while (k > 0) {
+			if (k % 2 == 1) val = matrixMultiply(val, m);
+			m  = matrixMultiply(m, m);
+			k /= 2;
+		}
+		if (n % 2 == 1) return val[0] + val[1]; 
+		return val[2] + val[3];
+	}
+	private static int[] matrixMultiply(int[] m, int[] n) {
+		return new int[] {m[0] * n[0] + m[1] * n[2],
+				  m[0] * n[1] + m[1] * n[3],
+				  m[2] * n[0] + m[3] * n[2],
+				  m[2] * n[1] + m[3] * n[3]};
 	}
 }
--- recursion/test_56.java
+++ recursion/test_56.java
@@ -1,8 +1,45 @@
 /*Write an algorithm to print all ways of arranging eight queens on a chess
 board so that none of them share the same row, column or diagonal*/
 
+import java.util.*;
+
 class test_56 {
 	public static void main(String[] args) {
-	
+		Queen(8);
+	}
+	public static void Queen(int n) {
+		ArrayList<int[]> all = new ArrayList<int[]>();
+		int[] board = new int[n];
+		placeQueen(0, n, board, all);
+		int total = 0;
+		for (int[] j : all) {
+			for (int i = 0; i < n; ++i) System.out.print("(" + i + ", " + j[i] + "), ");
+			System.out.println("");
+			total += 1;
+		}
+		System.out.println("There are " + total + " solutions to this questions.");
+	}
+	private static void placeQueen(int row, int n, int[] board, ArrayList<int[]> all) {
+		if (row == n) {
+			int[] tmp = new int[n];
+			for (int i = 0; i < n; ++i) {
+				tmp[i] = board[i];
+				System.out.println(board[i]);
+			}
+			all.add(tmp);
+			board = new int[n];
+			return;
+		}
+		for (int i = 0; i < n; ++i) {
+			board[row] = i;
+			if (check(row, board)) placeQueen(row + 1, n, board, all);
+		}
+	}
+	private static boolean check(int row, int[] board) {
+		for (int i = 0; i < row; ++i) {
+			int diff = Math.abs(board[i] - board[row]);
+			if (diff == 0 || diff == row - i) return false;
+		}
+		return true;
 	}
 }
