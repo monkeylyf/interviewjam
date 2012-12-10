6e196599fdd78d345073d8caba10693f2762eebc
monkeylyf <laituan1986@gmail.com>
2012-12-09 17:42:09 -0800

    added solution to test_50 and the followup question

--- recursion/test_50.java
+++ recursion/test_50.java
@@ -1,44 +1,54 @@
-/*Imagine a robot sitting on the upper left hand corner of an NxN grid The robot can
-only move in two directions: right and down. How many possible paths are there for the
-robot?
+/*Imagine a robot sitting on the upper left hand corner of an NxN grid. The
+robot can only move in two directions: right and down. How many possible paths
+are there for the robot?
 FOLLOW UP
-Imagine certain squares are “off limits”, such that the robot can not step on them.
-Design an algorithm to get all possible paths for the robot*/
+Imagine certain squares are “off limits”, such that the robot can not step
+on them. Design an algorithm to get all possible paths for the robot.
+
+Dynamic programming
+C(x, y) = C(x - 1, y) + C(x, y - 1)
+*/
 
 import java.util.*;
 
 
-
 class test_50 {
 	public static void main(String[] args) {
-		System.out.println("Does this work?");
-		Grid g = new Grid();
-	}
-}
-
-class Grid {
-	ArrayList<Point> cur_path = new ArrayList<Point>();
-
-	public static boolean getPaths(int x, int y) {
-		Point p = new Point(x, y);
-		cur_path.add(p);
-		if (x == 0 && y == 0) return true;
-		boolean success = false;
-		if (x >= 1 && is_free(x - 1, y)) {success = getPaths(x - 1, y);}
-		if (!success && y >= 1 && is_free(x, y - 1)) {success = getPaths(x, y - 1);}
-		if (!success) {cur_path.remove(p);}
-	}
-
-	public static is_free(int x, int y) {
-		
-	}
-}
-
-class Point {
-	int x;
-	int y;
-	public Point(int xx, int yy) {
-		x = xx;
-		y = yy;
-	}
+       my(3, 3);
+       ArrayList<int[]> ol = new ArrayList<int[]>();
+       ol.add(new int[] {1,1});
+       ol.add(new int[] {1,0});
+       myOffLimits(3, 3, ol);
+    }
+    public static void my(int x, int y) {
+        ArrayList<int[]> sol = new ArrayList<int[]>();
+        int[] path = new int[x + y - 2];
+        nextGrid(x - 1, y - 1, sol, path); // (x-1,y-1) is destination.
+        System.out.println("Total # of possible paths are: " + sol.size());
+    }
+    public static void nextGrid(int x, int y, ArrayList<int[]> sol, int[] path) {
+        if (x == 0 && y == 0) sol.add(path);
+        else {
+            if (x != 0) nextGrid(x - 1, y, sol, path);
+            if (y != 0) nextGrid(x, y - 1, sol, path);
+        }
+    }
+    // Followup.
+    public static void myOffLimits(int x, int y, ArrayList<int[]> ol) {
+        ArrayList<int[]> sol = new ArrayList<int[]>();
+        int[] path = new int[x + y - 2];
+        next(x - 1, y - 1, sol, path, ol); // (x-1,y-1) is destination.
+        System.out.println("Total # of possible paths are: " + sol.size());
+    }
+    public static void next(int x, int y, ArrayList<int[]> sol, int[] path, ArrayList<int[]> ol) {
+        if (x == 0 && y == 0) sol.add(path);
+        else {
+            if (isOffLimit(ol, x, y - 1) && y != 0) next(x, y - 1, sol, path, ol);      
+            if (isOffLimit(ol, x - 1, y) && x != 0) next(x - 1, y, sol, path, ol);      
+        }
+    }
+    public static boolean isOffLimit(ArrayList<int[]> arr, int x, int y) {
+        for (int[] i : arr) if (i[0] == x && i[1] == y) return false;
+        return true;
+    }
 }
