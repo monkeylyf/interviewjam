2d0256b94d8a90982f20fa8a12d5515157e4ae5e
monkeylyf <laituan1986@gmail.com>
2012-12-10 12:06:30 -0800

    added solution to test_54

--- recursion/test_54.java
+++ recursion/test_54.java
@@ -1,9 +1,36 @@
-/*Implement the “paint fill” function that one might see on many image editing programs.
-That is, given a screen (represented by a 2-dimensional array of Colors), a point, and
-a new color, fill in the surrounding area until you hit a border of that color*/
+/*Implement the "paint fill" function that one might see on many image editing
+programs.That is, given a screen (represented by a 2-dimensional array of
+Colors), a point, and a new color, fill in the surrounding area until you hit a
+border of that color.*/
+
 
 class test_54 {
 	public static void main(String[] args) {
-	
+        int[][] screen = new int[][] {{0, 0, 0, 0, 0, 0}, 
+                                      {0, 0, 0, 0, 0, 0},
+                                      {0, 0, 0, 0, 0, 0},
+                                      {0, 0, 1, 1, 0, 0},
+                                      {0, 0, 0, 0, 0, 0},
+                                      {0, 0, 0, 0, 0, 0}
+                                     };
+        paintFill(screen, 2, 2, 1);
 	}
+    public static void paintFill(int[][] screen, int x, int y, int tcolor) {
+        int row = screen[0].length;
+        int col = screen.length;
+        int ocolor = screen[x][y];
+        paintNext(screen, x, y, tcolor, ocolor, row, col);
+    }
+    public static void paintNext(int[][] screen, int x, int y, int tcolor, int ocolor, int row, int col) {
+        if ( x < 0 || x >= col || y < 0 || y >= row || screen[x][y] != ocolor) {
+            System.out.println("Reaching border of color " + ocolor + " @(" + x + ", " + y + ")");
+            return;
+        }
+        screen[x][y] = tcolor;
+        System.out.println("Painting (" + x + ", " + y + ") color " + tcolor);
+        paintNext(screen, x - 1, y, tcolor, ocolor, row, col);
+        paintNext(screen, x, y + 1, tcolor, ocolor, row, col);
+        paintNext(screen, x + 1, y, tcolor, ocolor, row, col);
+        paintNext(screen, x, y - 1, tcolor, ocolor, row, col);
+    }
 }
