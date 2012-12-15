21c192024654e8de57d44569d72921e2816c4709
monkeylyf <laituan1986@gmail.com>
2012-12-15 11:00:18 -0800

    test_20 revised

--- tree/test_20.java
+++ tree/test_20.java
@@ -4,6 +4,7 @@ differ in distance from the root by more than one*/
 
 class test_20 {
 	public static void main(String[] args) {
+        // Create tree.
 		Tree t = new Tree();
 		t.insert(1);
 		t.insert(2);
@@ -12,50 +13,38 @@ class test_20 {
 		t.insert(-8);
 		t.insert(-5);
 		t.insert(5);
-		t.inorder(t.root);
-		System.out.println("---------------");
-		t.preorder(t.root);
-		System.out.println("---------------");
-		t.postorder(t.root);
-		System.out.println("---------------");
 		System.out.println(t.maxDepth(t.root));
-		System.out.println("---------------");
 		System.out.println(t.minDepth(t.root));
-		System.out.println("---------------");
 		System.out.println(t.isBalanced(t.root));
 	}
 }
 
+
 class Tree {
 	public Node root;
-
-	// Find
+	// Find.
 	public Node findNext(Node entry, int key) {
 		if (entry == null) return null;
 		if (entry.data == key) return entry;
-		if (key < entry.data) {return findNext(entry.l, key);
-		} else {return findNext(entry.r, key);}
+		if (key < entry.data) return findNext(entry.l, key);
+		else return findNext(entry.r, key);
 	}
-
-	public Node find(int key) {return findNext(root, key);}
-
-	// Insert 
+	public Node find(int key) {
+        return findNext(root, key);
+    }
+	// Insert.
 	public void insert(int key) {
 		Node node = new Node();
 		node.data = key;
 		root = insertNext(root, node);
 	}
-
 	public Node insertNext(Node entry, Node node) {
-		if (entry == null) {entry = node;
-		} else if (entry.data > node.data ) {
-			// Link the parent node to the new node.
-			entry.l = insertNext(entry.l, node);
-		} else {entry.r = insertNext(entry.r, node);}
+		if (entry == null) entry = node;
+		else if (entry.data > node.data ) entry.l = insertNext(entry.l, node);
+		else entry.r = insertNext(entry.r, node);
 		return entry;
 	}
-
-	// Traverse.
+	// In-, pre-, post-order traverse.
 	public void inorder(Node entry) {
 		if (entry != null) {
 			inorder(entry.l);
@@ -63,35 +52,31 @@ class Tree {
 			inorder(entry.r);
 		}
 	}
-
 	public void preorder(Node entry) {
 		if (entry != null) {
 			System.out.println(entry.data);
-			inorder(entry.l);
-			inorder(entry.r);
+			preorder(entry.l);
+			preorder(entry.r);
 		}
 	}
-
 	public void postorder(Node entry) {
 		if (entry != null) {
-			inorder(entry.l);
-			inorder(entry.r);
+			postorder(entry.l);
+			postorder(entry.r);
 			System.out.println(entry.data);
 		}
 	}
-
+    // isBalanced.
 	public int maxDepth(Node entry) {
 		if (entry == null) return 0;
 		return 1 + Math.max(maxDepth(entry.r), maxDepth(entry.l));
 	}
-
 	public int minDepth(Node entry) {
 		if (entry == null) return 0;
 		return 1 + Math.min(minDepth(entry.r), maxDepth(entry.l));
 	}
-
 	public boolean isBalanced(Node entry) {
-		return (maxDepth(root) -  minDepth(root) <= 1);
+		return (maxDepth(root) <= minDepth(root) + 1);
 	}
 }
 
