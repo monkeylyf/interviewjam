c89b679ef5875bd176532caf89e1fb24680504cf
monkeylyf <laituan1986@gmail.com>
2012-12-16 10:42:40 -0800

    extra_1 revised. Added a solution to min cost with less time complex and space complex

--- dynamic_programming/extra_1.java
+++ dynamic_programming/extra_1.java
@@ -13,41 +13,35 @@ import java.util.*;
 
 class extra_1 {
     public static void main(String args[]) {
-        int[] two = new int[] {0,0,1,0,2,3,1,3,2,0,3}; // two[0]=two[1]=0;two[2]:0-2;
-        int[] one = new int[] {0,1,2,2,1,2,2,1,2,2,1}; // one[0]=0; one[1]:0-1;one[2]:1-2;
+        //int[] two = new int[] {0,0,1,0,2,3,1,3,2,0,3}; // two[0]=two[1]=0;two[2]:0-2;
+        //int[] one = new int[] {0,1,2,2,1,2,2,1,2,2,1}; // one[0]=0; one[1]:0-1;one[2]:1-2;
+        int[] two = new int[] {0,0,1,2,3};
+        int[] one = new int[] {0,1,1,1,1};
         //myPath(two, one);
-        myCost(two, one);
+        //myCost(two, one);
+        my(two, one);
     }
     // Get the path of min cost.
-    public static void myPath(int[] two, int[] one) {
+    /*public static void myPath(int[] two, int[] one) {
         ArrayList<Integer> path = new ArrayList<Integer>();
-        nextStep(two, one, two.length - 1, path);
+        nextStep(two, one, two.length - 1, "", "", "");
         for (int i : path) System.out.print(i);
         System.out.println("");
     }
-    public static int nextStep(int[] two, int[] one, int x, ArrayList<Integer> path) {
-        if (x == 0) {
-            System.out.println("Reaching the origin!");
-            return 0;
-        }
-        if (x == 1) {
-            System.out.println("To point " + x + " take 1 step");
-            path.add(one[x]);
-            return one[x];
-        } else {
+    public static int nextStep(int[] two, int[] one, int x, String step, String t, String o) {
+        if (x == 0) {return 0; step += "0-0";}
+        if (x == 1) {return one[x]; step += "0-1";}
+        else {
             int take_two = nextStep(two, one, x - 2, path);
             int take_one = nextStep(two, one, x - 1, path);
             if ((take_one + one[x]) > (take_two + two[x])) {
-                System.out.println("To point " + x + " take 2 steps");
-                path.add(two[x]);
+                step = "";
                 return take_two;
             } else {
-                System.out.println("To point " + x + " take 1 step");
-                path.add(one[x]);
                 return take_one;
             }
         }
-    }
+    }*/
     // Get the min Cost.
     public static void myCost(int[] two, int[] one) {
         System.out.println("The min cost is: " + nextCost(two, one, two.length - 1));
@@ -55,5 +49,19 @@ class extra_1 {
     public static int nextCost(int[] two, int[] one, int x) {
         if (x < 2) return one[x];
         return Math.min(nextCost(two, one, x - 1) + one[x], nextCost(two, one, x - 2) + two[x]);
+    }
+    // Get the min Cost with less time and space complex.
+    public static void my(int[]two, int[] one) {
+        int two_before = 0;
+        int one_before = one[1];
+        int index = 2;
+
+        while (index < one.length) {
+            int tmp = Math.min(one_before + one[index], two_before + two[index]);
+            two_before = one_before;
+            one_before = tmp;
+            ++index;
+        }
+        System.out.println("The min cost is: " + one_before);
     }
 }
