d1cadd6399c2ca183fe8f2263a0f4c6519466c9b
monkeylyf <laituan1986@gmail.com>
2012-12-15 16:35:06 -0800

    test_23 revised

--- tree/test_23.java
+++ tree/test_23.java
@@ -17,87 +17,65 @@ class test_23 {
 		t.insert(3);
 		t.insert(5);
 		t.insert(7);
-		System.out.println("----------------");
-		t.bft();
-		System.out.println("----------------");
-		t.dft();
-		System.out.println("----------------");
-		//ArrayList<LinkedList<Node>> arr = t.breadthFirstCount();
-		t.breadthFirstCount();
-		System.out.println("----------------");
-		//for (LinkedList<Node> ll : arr) {
-		//	for (Node n : ll) {System.out.println(n.data);}
-		//}
+        // Test case.
+		ArrayList<LinkedList<Node>> arr = t.breadthFirstCount();
+		for (LinkedList<Node> ll : arr) {
+			for (Node n : ll) System.out.print(n.data + " ");
+            System.out.println("");
+		}
 	}
 }
 
 class Tree {
 	public Node root;
-
 	public void insert(int d) {
 		Node newNode = new Node();
 		newNode.data = d;
 		root = getNext(root, newNode);
 	}
-
 	public Node getNext(Node entry, Node node) {
 		if (entry == null) entry = node;
-		else if (node.data < entry.data) {entry.l = getNext(entry.l, node);}
+		else if (node.data < entry.data) entry.l = getNext(entry.l, node);
 		else entry.r = getNext(entry.r, node); 
 		return entry;
 	}
-
 	public void bft() {
 		Queue<Node> unvisited = new LinkedList<Node>();
-		Queue<Node> visited = new LinkedList<Node>();
 		unvisited.add(root);
-		while(!unvisited.isEmpty()) {
+		while (!unvisited.isEmpty()) {
 			Node cur = unvisited.remove();
 			if(cur.l != null) unvisited.add(cur.l);
 			if(cur.r != null) unvisited.add(cur.r);
 			System.out.println(cur.data);
 		}
 	}
-
 	public void dft() {
 		Stack<Node> unvisited = new Stack<Node>();
 		unvisited.push(root);
-		while(!unvisited.isEmpty()) {
+		while (!unvisited.isEmpty()) {
 			Node cur = unvisited.pop();
 			if(cur.r != null) unvisited.push(cur.r);
 			if(cur.l != null) unvisited.push(cur.l);
 			System.out.println(cur.data);
 		}
 	}
-
 	public ArrayList<LinkedList<Node>> breadthFirstCount() {
 		ArrayList<LinkedList<Node>> result = new ArrayList<LinkedList<Node>>();
 		Queue<Node> unvisited = new LinkedList<Node>();
-		Queue<Node> temp = new LinkedList<Node>();
-
-		unvisited.add(root);
-		int layer = 1;
-		int cnt = 0;
 		LinkedList<Node> ll = new LinkedList<Node>();
-		while(true) {
+		unvisited.add(root);
+        int count = 1;
+		while (true) {
 			Node node = unvisited.remove();
-			temp.add(node);
 			ll.add(node);
-			++cnt;
-			if (unvisited.isEmpty()) {
-				System.out.println("Layer " + layer + " has " + cnt + " nodes:");
-				for (Node n : ll) {System.out.println(n.data);}
-				result.add(ll.clone());
-				ll.clear();
-				++layer;
-				cnt = 0;
-				while (!temp.isEmpty()) {
-					Node cur = temp.remove();
-					if (cur.l != null) unvisited.add(cur.l);
-					if (cur.r != null) unvisited.add(cur.r);
-				}
-			}
-			if (unvisited.isEmpty() && temp.isEmpty()) break;
+            if (node.l != null) unvisited.add(node.l);
+            if (node.r != null) unvisited.add(node.r);
+            if (--count == 0) {
+                result.add(ll);
+                ll = new LinkedList<Node>();
+                count = unvisited.size();
+            }
+            if (unvisited.isEmpty()) break;
 		}
 		return result;
 	}
