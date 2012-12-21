12590dbb5519b15877f08cddb4db43463eaef36f
monkeylyf <laituan1986@gmail.com>
2012-12-21 12:03:27 -0800

    leetcode_5 and test_24 are identical. updated test_24 after updatingn leetcode_5

--- linkedlist/test_12.java
+++ linkedlist/test_12.java
@@ -15,20 +15,52 @@ class test_12 {
 		Node n2 = new Node(2);
 		n2.addToTail(9);
 		n2.addToTail(5);
+        Node retval = addTwo(n1, n2);
+        while (retval != null) {
+            System.out.println(retval.data);
+            retval = retval.next;
+        }
 	}
-
-	public static Node my(boolean offset, Node a, Node b) {
-		if (a == null && b == null) {return nulll;}
-		Node result = new Node();
-		int v
-		if (1 != null) {value += }
+	public static Node addTwo(Node a, Node b) {
+        Node head = new Node(0);
+        Node cur = head;
+        int carry = 0;
+        while (true) {
+            if (a != null && b != null) {
+                cur.next = new Node((a.data + b.data + carry) % 10);
+                carry = (a.data + b.data + carry) / 10;
+                cur = cur.next;
+                a = a.next;
+                b = b.next;
+            } else if (a == null && b == null) {
+                if (carry == 1) cur.next = new Node(1);
+                break;
+            } else {
+                if (a == null) {
+                    cur.next = new Node((b.data + carry) % 10);
+                    carry = (b.data + carry) / 10;
+                    cur = cur.next;
+                    b = b.next;
+                } else {
+                    cur.next = new Node((a.data + carry) % 10);
+                    carry = (a.data + carry) / 10;
+                    cur = cur.next;
+                    a = a.next;
+                }
+            }
+        }
+        return head.next;
 	}
 }
 
+
 class Node {
-	Node next = null;
+	Node next;
 	int data;
-	public Node(int d) {data = d;}
+	public Node(int d) {
+        data = d;
+        next = null;
+    }
 	public void addToTail(int d) {
 		Node node = this;
 		while (node.next != null) {node = node.next;}
