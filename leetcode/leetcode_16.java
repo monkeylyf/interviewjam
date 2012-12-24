99ee9ab666787991593f526b8402c2de2874d705
monkeylyf <laituan1986@gmail.com>
2012-12-23 17:57:50 -0800

    added solution to leetcode_16

---
+++ leetcode/leetcode_16.java
@@ -0,0 +1,21 @@
+/*You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can
+you climb to the top?*/
+
+
+class leetcode_16 {
+    public static void main(String[] args) {
+    
+    }
+    public static int climbStairs(int n) {
+        int f0 = 0; // if n = 0, there is no way.
+        int f1 = 1; // if n = 1, there is only one way.
+        int retval = 0;
+        for (int i = 0; i < n; ++i) {
+            retval = f0 + f1; // C(n) = C(n-1) + C(n-2)
+            f0 = f1;
+            f1 = retval;
+        }
+        return retval;
+    }
+}
