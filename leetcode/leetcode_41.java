d6d067bb49edaf96cba22a6bacf0d4a734c7ba67
monkeylyf <laituan1986@gmail.com>
2012-12-28 17:14:12 -0800

    added solution to leetcode_41

---
+++ leetcode/leetcode_41.java
@@ -0,0 +1,25 @@
+/*Given a string s consists of upper/lower-case alphabets and empty space
+characters ' ', return the length of last word in the string.
+If the last word does not exist, return 0.
+Note: A word is defined as a character sequence consists of non-space characters only.
+For example, 
+Given s = "Hello World",
+return 5.*/
+
+
+class leetcode_41 {
+    public static void main(String[] args) {
+    }
+    public static int lengthOfLastWord(String s) {
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            char cur = s.charAt(i);
+            if (cur != ' ') {
+                ++count;
+            } else {
+                if (count != 0) break;
+            }
+        }
+        return count;
+    }
+}
