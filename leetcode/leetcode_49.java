7a6e09d77819db1f4e4ede049a4e3b1f6684ffe1
monkeylyf <laituan1986@gmail.com>
2012-12-30 20:06:13 -0800

    added solution to leetcode_49

---
+++ leetcode/leetcode_49.java
@@ -0,0 +1,29 @@
+/*Find the contiguous subarray within an array (containing at least one number)
+which has the largest sum.
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+More practice:
+If you have figured out the O(n) solution, try coding another solution using
+the divide and conquer approach, which is more subtle.
+*/
+
+
+class leetcode_49 {
+    public static void main(String[] args) {
+    }
+    public static int maxSubArray(int[] A) {
+        boolean noPositive = true;
+        int sum = 0;
+        int max = 0;
+        int localMax = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; ++i) {
+            if (A[i] > 0) noPositive = false;
+            if (A[i] > localMax) localMax = A[i];
+            sum += A[i];
+            if (sum > max) max = sum;
+            else if (sum < 0) sum = 0;
+        }
+        if (noPositive) return localMax;
+        else return max;
+    }
+}
