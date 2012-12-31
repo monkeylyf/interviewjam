e105185bf44e2392af7b26db471ce97d259d728f
monkeylyf <laituan1986@gmail.com>
2012-12-31 14:53:02 -0800

    added solution to leetcode_52

---
+++ leetcode/leetcode_52.java
@@ -0,0 +1,82 @@
+/*Populating next right pointers in each node
+
+Given a binary tree
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
+
+Populate each next pointer to point to its next right node. If there is no next
+right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+Note:
+You may only use constant extra space.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same
+level, and every parent has two children).
+For example,
+Given the following perfect binary tree,
+
+        1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+After calling your function, the tree should look like:
+
+        1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \  / \
+    4->5->6->7 -> NULL
+*/
+
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+class leetcode_52 {
+    public static void main(String[] args) {
+        TreeLinkNode n1 = new TreeLinkNode(1);
+        TreeLinkNode n2 = new TreeLinkNode(2);
+        TreeLinkNode n3 = new TreeLinkNode(3);
+        n1.left = n2;
+        n1.right = n3;
+        connect(n1);
+    }
+    public static void connect(TreeLinkNode root) {
+        if (root == null) return;
+        Queue<TreeLinkNode> unvisited = new LinkedList<TreeLinkNode>();
+        LinkedList<TreeLinkNode> ll = new LinkedList<TreeLinkNode>();
+        unvisited.add(root);
+        int count = 1;
+        while (true) {
+            TreeLinkNode node = unvisited.remove();
+            ll.add(node);
+            if (node.left != null) unvisited.add(node.left);
+            if (node.right != null) unvisited.add(node.right);
+            if (--count == 0) {
+                for (int i = 0; i < ll.size(); ++i) {
+                    if (i + 1 < ll.size()) {
+                        ll.get(i).next = ll.get(i + 1);
+                    }
+                }
+                ll = new LinkedList<TreeLinkNode>();
+                count = unvisited.size();
+            }
+            if (unvisited.isEmpty()) break;
+        }
+    }
+}
+
+
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
