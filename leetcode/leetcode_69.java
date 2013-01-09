1b3fe8b9795644e7cde3a4991d22e2a120820659
monkeylyf <laituan1986@gmail.com>
2013-01-08 19:40:38 -0800

    added solution to leetcode_69

---
+++ leetcode/leetcode_69.java
@@ -0,0 +1,41 @@
+/*Permutations
+
+Given a collection of numbers, return all possible permutations.
+For example,
+[1,2,3] have the following permutations:
+[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+*/
+
+import java.util.ArrayList;
+
+
+class leetcode_69 {
+    public static void main(String[] args) {
+        for (ArrayList<Integer> i : permute(new int[] {0, 1})) System.out.println(i);
+        for (ArrayList<Integer> i : permute(new int[] {0, -1, 1})) System.out.println(i);
+    }
+    public static ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> all = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+        boolean[] used = new boolean[num.length];
+        nextNum(num, used, tmp, all);
+        return all;
+    }
+    public static void nextNum(int[] num, boolean[] used, ArrayList<Integer> tmp, ArrayList<ArrayList<Integer>> all) {
+        if (tmp.size() == num.length) {
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            for (int i : tmp) res.add(num[i]);
+            all.add(res);
+        } else {
+            for (int i = 0; i < num.length; ++i) {
+                if (!used[i]) {
+                    tmp.add(i);
+                    used[i] = true;
+                    nextNum(num, used, tmp, all);
+                    tmp.remove(tmp.size() - 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
