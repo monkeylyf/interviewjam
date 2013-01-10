dfcd409f0706fefbef36a1d9b963c2a55877fc94
monkeylyf <laituan1986@gmail.com>
2013-01-09 17:09:24 -0800

    added solution to leetcode_75

---
+++ leetcode/leetcode_75.java
@@ -0,0 +1,53 @@
+/*Recover Binary Search Tree
+
+Two elements of a binary search tree (BST) are swapped by mistake.
+Recover the tree without changing its structure.
+Note:
+A solution using O(n) space is pretty straight forward. Could you devise a
+constant space solution?
+*/
+
+import java.util.ArrayList;
+
+class leetcode_75 {
+    public static void main(String[] args) {
+    }
+    public static void recoverTree(TreeNode root) {
+        if (root == null) return;
+        ArrayList<TreeNode> inorder = new ArrayList<TreeNode>();
+        inorderTraverse(root, inorder);
+        TreeNode nodeOne = null;
+        TreeNode nodeTwo = null;
+        for (int i = 0; i < inorder.size() - 1; ++i) {
+            if (inorder.get(i).val > inorder.get(i + 1).val) {
+                nodeOne = inorder.get(i);
+                break;
+            }
+        }
+        for (int i = inorder.size() - 1; i > 0; --i) {
+            if (inorder.get(i).val < inorder.get(i - 1).val) {
+                nodeTwo = inorder.get(i);
+                break;
+            }
+        }
+        int swap = nodeOne.val;
+        nodeOne.val = nodeTwo.val;
+        nodeTwo.val = swap;
+    }
+    public static void inorderTraverse(TreeNode root, ArrayList<TreeNode> inorder) {
+        if (root == null) return;
+        inorderTraverse(root.left, inorder);
+        inorder.add(root);
+        inorderTraverse(root.right, inorder);
+    }
+}
+
+
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+    TreeNode(int x) {
+        val = x;
+    }
+}
