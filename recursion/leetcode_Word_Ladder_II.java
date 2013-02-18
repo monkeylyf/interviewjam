e4e377facd449d1644955f2f5795488f3cf2e55f
monkeylyf <laituan1986@gmail.com>
2013-02-18 12:08:45 -0800

    finally solved leetcode_Word_Ladder_II!!!!

--- recursion/leetcode_Word_Ladder_II.java
+++ recursion/leetcode_Word_Ladder_II.java
@@ -23,224 +23,95 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.HashSet;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Iterator;
 
 
 class leetcode_Word_Ladder_II {
     public static void main(String[] args) {
-        HashSet<String> dict = new HashSet<String>();
-        Collections.addAll(dict, "ted","tex","red","tax","tad","den","rex","pee");
-        //System.out.println(findLadders("hot", "dog", dict));
-        my("red", "tax", dict);
+        HashSet<String> dict1 = new HashSet<String>();
+        Collections.addAll(dict1, "ted","tex","red","tax","tad","den","rex","pee");
+        test("red", "tax", dict1);
+        HashSet<String> dict2 = new HashSet<String>();
+        Collections.addAll(dict2, "hot", "dot");
+        test("hot", "hot", dict2);
     }
-    public static ArrayList<ArrayList<String>> my(String start, String end, HashSet<String> dict) {
-        ArrayList<ArrayList<String>> all = new ArrayList<ArrayList<String>>();
-        Queue<String> unvisited = new LinkedList<String>();
-        HashSet<String> visited = new HashSet<String>();
-        ArrayList<String> visitedCache = new ArrayList<String>();
-        Hashtable<String, ArrayList<String>> path = new Hashtable<String, ArrayList<String>>();
-        unvisited.add(start); // Init unvisited queue.
-        visited.add(start);
-        int count = 1; // Counter for the number of unvisited node at next level.
+    public static ArrayList<ArrayList<String>> test(String start, String end, HashSet<String> dict) {
+        // The idea behind this is BFS + hashing paths.
+        // Since the shortest path is aksed for then DFS is not an efficienty way to solve this.
+        // Need two HashMap path & tmPath, one for global path and another for path at current level.
+        // Need two Queue curLevel & nextTovVisit to record graph level.
+        HashMap<String, ArrayList<String>> path = new HashMap<String, ArrayList<String>>();
+        HashMap<String, ArrayList<String>> tmpPath = new HashMap<String, ArrayList<String>>();
+        Queue<String> curLevel = new LinkedList<String>();
+        Queue<String> nextLevel = new LinkedList<String>();
+        curLevel.add(start);
         boolean isShortestLevel = false;
-        while (!unvisited.isEmpty()) {
-            String str = unvisited.remove();
-            System.out.println("current String " + str);
-            for (String cur : generate(str, dict)) {
-                if (cur.equals(end)) {
-                    System.out.println("Found end " + end);
-                    ArrayList<String> res = new ArrayList<String>();
-                    res.add(str);
-                    res.add(end);
-                    nextPath(str, path, all, res);
-                    isShortestLevel = true;
-                } else if (!visited.contains(cur)) {
-                    if (!visitedCache.contains(cur)) {
-                        unvisited.add(cur);
+        while (!curLevel.isEmpty()) {
+            String str = curLevel.remove();
+            for (int i = 0; i < start.length(); ++i) {
+                for(char j = 'a'; j <= 'z'; ++j){
+                    // Change the str then check if the dict contains it.
+                    char[] chs = str.toCharArray();
+                    chs[i] = j;
+                    String oneCharDiff = new String(chs);
+                    if (oneCharDiff.equals(str)) {
+                        continue;
                     }
-                    // Mark start as visited. If one of a string's one-char-diff strings
-                    // equals it, make sure don't jump to it since we need the shortest
-                    // path. no looping.
-                    visitedCache.add(cur);
-                    if (path.containsKey(cur)) {
-                        ArrayList<String> arr = path.get(cur);
-                        arr.add(str);
-                        System.out.println("Add existing key: " + cur + " ArrayList: " + arr);
-                        path.put(cur, arr);
-                    } else {
-                        ArrayList<String> arr = new ArrayList<String>();
-                        arr.add(str);
-                        System.out.println("Add key: " + cur + " ArrayList: " + arr);
-                        path.put(cur, arr);
+                    if (oneCharDiff.equals(end)) {
+                        isShortestLevel = true; // Reach the shortest path.
+                    }
+                    if (!path.containsKey(oneCharDiff) && dict.contains(oneCharDiff)) {
+                        if (!tmpPath.containsKey(oneCharDiff)) {
+                            ArrayList<String> arr = new ArrayList<String>();
+                            arr.add(str);
+                            tmpPath.put(oneCharDiff, arr);
+                            nextLevel.add(oneCharDiff);
+                        } else {
+                            ArrayList<String> arr = tmpPath.get(oneCharDiff);
+                            arr.add(str);
+                            tmpPath.put(oneCharDiff, arr);
+                        }
                     }
                 }
             }
-            if (--count == 0) { // Done with the cur level.
-                System.out.println("========changing level=========");
-                for (String i : visitedCache) {
-                    visited.add(i);
-                }
-                count = unvisited.size(); // Reset counter.
-                if (isShortestLevel) { // Done with all unvisited node in shortest level then return.
-                    for (ArrayList<String> i : all) System.out.println(i);
-                    return all;
+            if (curLevel.isEmpty()) {
+                Queue<String> tmp = nextLevel; // Swap nextLevel and curLevel.
+                nextLevel = curLevel;
+                curLevel = tmp;
+                path.putAll(tmpPath); // Move all paths hashed as this level to global path map.
+                tmpPath = new HashMap<String, ArrayList<String>>(); // tmpPath.clear()
+                if (isShortestLevel) {
+                    return dfsPath(path, start, end); // Finished with level with shortest path. Start reconstructing path.
                 }
             }
         }
+        return new ArrayList<ArrayList<String>>();
+    }
+    public static ArrayList<ArrayList<String>> dfsPath(HashMap<String, ArrayList<String>> path, String start, String end) {
+        // Standord DFS.
+        ArrayList<ArrayList<String>> all = new ArrayList<ArrayList<String>>();
+        ArrayList<String> one = new ArrayList<String>();
+        one.add(start);
+        nextPath(path, start, end, all, one, 0);
+        for (ArrayList<String> i : all) System.out.println(i);
         return all;
     }
-    public static void nextPath(String cur, Hashtable<String, ArrayList<String>> path, ArrayList<ArrayList<String>> all, ArrayList<String> res) {
-        ArrayList<String> nextArr = path.get(cur);
-        if (nextArr == null) {
+    public static void nextPath(HashMap<String, ArrayList<String>> path, String start, String end, ArrayList<ArrayList<String>> all, ArrayList<String> one, int level) {
+        // :param level: type int. for corner cases like "hot", "hot", ["hot", "dot"]
+        // At the first level start might equal to end, but the recursion should keep going.
+        if (end.equals(start) && level > 0) {
             ArrayList<String> tmp = new ArrayList<String>();
-            for (String i : res) {
+            for (String i : one) {
                 tmp.add(i);
             }
             all.add(tmp);
         } else {
-            for (String str : nextArr) {
-                res.add(0, str);
-                nextPath(str, path, all, res);
-                res.remove(0);
+            one.add(1, end);
+            for (String i : path.get(end)) {
+                nextPath(path, start, i, all, one, level + 1);
             }
+            one.remove(1);
         }
-    }
-    public static HashSet<String> generate(String s, HashSet<String> dict){
-        HashSet<String> words = new HashSet<String>();
-        for(int i = 0; i < s.length(); ++i){
-            for(char j = 'a'; j <= 'z'; ++j){
-                char[] chs = s.toCharArray();
-                chs[i] = j;
-                String ns = new String(chs);
-                if (ns.equals(s)) {
-                    continue;
-                } else if(dict.contains(ns))
-                    words.add(ns);
-                }
-            }
-        }
-        return words;
-    }
-    public static int ladderLength(String start, String end, HashSet<String> dict) {
-        // The idea behind this is Depth first search. And this will not pass leetcode
-        // onlinejudge Judge Large. Before the func jumps into the next recursion, it
-        // iterates through all element in dict, which is extremly inefficient when the
-        // size of dict is large.
-        // BFS is more efficient because. For each char in given word we find out the
-        // potential change by one char, then there is (26 - 1) * word.length() then check
-        // if they are in dict (time complexity should be O(1)), which is much smaller than
-        // iterating through dict to find the word differed by one char ( time complexity: 
-        // dict.size() * word.length())
-        // change on char of given word then look it up in dict VS iterate through dict and
-        // find the one with one char diff.
-        ArrayList<ArrayList<String>> all = new ArrayList<ArrayList<String>>();
-        ArrayList<String> tmp = new ArrayList<String>();
-        tmp.add(start);
-        int[] shortest = new int[] {Integer.MAX_VALUE};
-        nextChange(start, end, dict, all, tmp, shortest);
-        for (ArrayList<String> i : all) {
-            System.out.println(i);
-        }
-        return shortest[0] + 1;
-    }
-    public static void nextChange(String start, String end, HashSet<String> dict, ArrayList<ArrayList<String>> all, ArrayList<String> tmp, int[] shortest) {
-        if (diffByOneChar(start, end)) {
-            if (tmp.size() <= shortest[0]) {
-                ArrayList<String> answer = new ArrayList<String>();
-                for (String i : tmp) {
-                    answer.add(i);
-                }
-                answer.add(end);
-                if (tmp.size() < shortest[0]) {
-                    all.clear();
-                    shortest[0] = tmp.size();
-                }
-                all.add(answer);
-            }
-        } else {
-            ArrayList<String> mark = new ArrayList<String>();
-            Iterator itr = dict.iterator();
-            while (itr.hasNext()) {
-                String str = (String) itr.next();
-                if (diffByOneChar(str, start)) {
-                    mark.add(str);
-                }
-            }
-            for (String str : mark) {
-                dict.remove(str);
-                tmp.add(str);
-                nextChange(str, end, dict, all, tmp, shortest);
-                tmp.remove(tmp.size() - 1);
-                dict.add(str);
-            } 
-        }
-    }
-    public static boolean diffByOneChar(String a, String b) {
-        if (a.length() != b.length()) {
-            return false;
-        } else {
-            int diff = 0;
-            for (int i = 0; i < a.length(); ++i) {
-                if (a.charAt(i) != b.charAt(i)) {
-                    ++diff;
-                }
-                if (diff > 1) {
-                    return false;
-                }
-            }
-            return diff == 1;
-        }
-    }
-    // Still can not pass the onlinejudge Judge Large.
-    public static ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
-        ArrayList<ArrayList<String>> all = new ArrayList<ArrayList<String>>();
-        ArrayList<String> tmp = new ArrayList<String>();
-        tmp.add(start);
-        int[] shortest = new int[] {Integer.MAX_VALUE};
-        nextChar(start, end, dict, all, tmp, shortest);
-        for (ArrayList<String> i : all) {
-            System.out.println(i);
-        }
-        return all;
-    }
-    public static void nextChar(String start, String end, HashSet<String> dict, ArrayList<ArrayList<String>> all, ArrayList<String> tmp, int[] shortest) {
-        if (diffByOneChar(start, end)) {
-            if (tmp.size() <= shortest[0]) {
-                ArrayList<String> answer = new ArrayList<String>();
-                for (String i : tmp) {
-                    answer.add(i);
-                }
-                answer.add(end);
-                if (tmp.size() < shortest[0]) {
-                    all.clear();
-                    shortest[0] = tmp.size();
-                }
-                all.add(answer);
-            }
-        } else if (tmp.size() < shortest[0]) {
-            for (String j : diffByGivenChar(start)) {
-                if (dict.contains(j)) {
-                    dict.remove(j);
-                    tmp.add(j);
-                    nextChar(j, end, dict, all, tmp, shortest);
-                    tmp.remove(tmp.size() - 1);
-                    dict.add(j);
-                }
-            }
-        }
-    }
-    public static HashSet<String> diffByGivenChar(String word) {
-        HashSet<String> all = new HashSet<String>();        
-        for (int i = 0; i < word.length(); ++i) {
-            char[] arr = word.toCharArray();
-            for (char j = 'a'; j <= 'z'; ++j) {
-                if (word.charAt(i) != j) {
-                    arr[i] = j;
-                    all.add(new String(arr));
-                }
-            }
-        }
-        return all;
     }
 }
