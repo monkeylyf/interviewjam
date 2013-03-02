5065eb0cb908f990f95653f768ebb2bed5e86566
monkeylyf <laituan1986@gmail.com>
2013-03-02 11:34:41 -0800

    added leetcode_Palindrome_Partitioning

---
+++ str/leetcode_Palindrome_Partitioning.java
@@ -0,0 +1,67 @@
+/*Palindrome_Partitioning
+
+Given a string s, partition s such that every substring of the partition is a
+palindrome.
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+  [
+      ["aa","b"],
+      ["a","a","b"]
+  ]
+*/
+
+import java.util.ArrayList;
+
+
+class leetcode_Palindrome_Partitioning {
+    public static void main(String[] args) {
+        Solution test = new Solution("amanaplanacanalpanama");
+        System.out.println(test.partition());
+    }
+}
+
+
+class Solution {
+    private String s;
+    private int n;
+    private boolean[][] status;
+    Solution(String s) {
+        this.s = s;
+        this.n = s.length();
+        this.status = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            this.status[i][i] = true;
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            this.status[i][i + 1] = (s.charAt(i) == s.charAt(i + 1)) ? true : false;
+            for (int j = i + 2; j < n; ++j) {
+                this.status[i][j] = (s.charAt(i) == s.charAt(j) && status[i + 1][j - 1]) ? true : false;
+            }
+        }
+    }
+    public ArrayList<ArrayList<String>> partition() {
+        ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
+        ArrayList<String> tmp = new ArrayList<String>();
+        nextPalindrome(0, res, tmp);
+        return res;
+    }
+    private void nextPalindrome(int index, ArrayList<ArrayList<String>> res, ArrayList<String> tmp) {
+        if (index == this.n) {
+            ArrayList<String> copy = new ArrayList<String>();
+            for (String i : tmp) {
+                copy.add(i);
+            }
+            res.add(copy);
+        }
+        for (int i = index; i < this.n; ++i) {
+            if (this.status[index][i]) {
+                tmp.add(this.s.substring(index, i + 1));
+                nextPalindrome(i + 1, res, tmp);
+                tmp.remove(tmp.size() - 1);
+            }
+        } 
+    }
+}
