669e1d00a15ab4082e6ec6b3ab8603242c636975
monkeylyf <laituan1986@gmail.com>
2013-03-02 10:38:03 -0800

    mv some palindrome related questions under str dir

--- arr/leetcode_Trapping_Rain_Water.java
+++ arr/leetcode_Trapping_Rain_Water.java
@@ -15,18 +15,20 @@ class leetcode_Trapping_Rain_Water {
         trap(new int[] {0,1,0,2,1,0,1,3,2,1,2,1});
     }
     public static int trap(int[] A) {
+        // Time complexity is O(n) since pop() and push() operation happens once each.
+        // The idea behind this is find the right higher bank first then mark the lower.
+        // Then find the left higher bank, trapped water = (Math.min(left high, right high) - lower) * width.
         Stack<Integer> s = new Stack<Integer>();
         int res = 0;
         int prev, lowBank, highBank;
         for (int i = 0; i < A.length; ++i) {
-            while (!s.isEmpty() && A[i] > A[s.peek()]) { // cur A[i] is higher than previous.
-                // Can hold some water.
-                lowBank = A[s.pop()];
-                // Remember to check if the stack is empty or not
-                // also, only add water when i < n.
+            // The while loop here makes sure that stack s contains only decreasing int sequence
+            // increasing sequence means water might be trapped.
+            while (!s.isEmpty() && A[i] > A[s.peek()]) { // right higher bank detected
+                lowBank = A[s.pop()]; // lower bottom
                 if (!s.isEmpty()) {
                     prev = s.peek();
-                    highBank = Math.min(A[i], A[prev]);
+                    highBank = Math.min(A[i], A[prev]); // left hihger bank
                     res += (highBank - lowBank) * (i - prev - 1);
                 }
             }
