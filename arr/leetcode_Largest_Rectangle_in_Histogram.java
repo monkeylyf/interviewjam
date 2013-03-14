ee1fc374ff141acf48bd95414011bedd1ebde190
monkeylyf <laituan1986@gmail.com>
2013-03-13 17:20:41 -0700

    revised leetcode_Largest_Rectangle_in_Histogram

--- arr/leetcode_Largest_Rectangle_in_Histogram.java
+++ arr/leetcode_Largest_Rectangle_in_Histogram.java
@@ -14,32 +14,33 @@ import java.util.Stack;
 
 class leetcode_Largest_Rectangle_in_Histogram {
     public static void main(String[] args) {
-        largestRectangleArea(new int[] {2, 1, 5, 6, 2, 3});
+        System.out.println(largestRectangleArea(new int[] {2, 1, 5, 6, 5, 2, 3}));
     }
     public static int largestRectangleArea(int[] height) {
-        if (height.length == 0) {
-            return 0;
-        }
-        int result = 0;
-        Stack<Integer> indexes = new Stack<Integer>();
-        Stack<Integer> heights = new Stack<Integer>();
-        for (int i = 0; i < height.length; i++) {
-            if (heights.isEmpty() || height[i] > heights.peek()) {
+        /* 1. Brute force: O(n^3), enumerate left and right border and calculate the area.
+         * 2. Using maxHeap: O(n^2 logn? not sure), enumerate left & right and push into maxHeap and peek the min.
+         * 3. Monotone priority stack: O(n)
+         */
+        int result = 0, lastIndex = 0, lastHeight = 0;
+        Stack<Integer> indexes = new Stack<Integer>(); // Monotone priority stack to trace index.
+        Stack<Integer> heights = new Stack<Integer>(); // Monotone priority stack to trace height.
+        for (int i = 0; i < height.length; ++i) {
+            if (heights.isEmpty() || height[i] > heights.peek()) { // Increasing height, push index and height.
                 heights.push(height[i]);
                 indexes.push(i);
-            } else if (height[i] < heights.peek()) {
-                int lastIndex = 0;
-                int lastHeight = 0;
+            } else if (height[i] < heights.peek()) { // Decrease    
                 while (!heights.isEmpty() && heights.peek() > height[i]) {
                     lastIndex = indexes.pop();
                     lastHeight = heights.pop();
                     result = Math.max(result, lastHeight * (i - lastIndex));
                 }
                 heights.push(height[i]);
+                // Tricky part: instead of pushing(i), it pushes lastIndex, which is min index
+                // height[i] can extent leftmost.
                 indexes.push(lastIndex);
             }
         }
-        while (!heights.isEmpty()) {
+        while (!heights.isEmpty()) { // Last element as right border, update result.
             result = Math.max(result, heights.pop() * (height.length - indexes.pop()));
         }
         return result;
