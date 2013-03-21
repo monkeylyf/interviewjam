d9e920dd42cd35216599cdbd0081a600d8cf9db4
monkeylyf <laituan1986@gmail.com>
2013-03-20 22:33:46 -0700

    passed hackerrank_Lego_Blocks.java

--- dynamic_programming/hackerrank_Lego_Blocks.java
+++ dynamic_programming/hackerrank_Lego_Blocks.java
@@ -34,76 +34,76 @@ of ways is 2 * 2 * 2 - 1 = 7.
 PS:”aa” is one lego block of size 1 * 1 * 2, “b” and “c” are lego blocks of size 1 * 1 * 1.
 */
 
-
-import java.util.*;
-
+import java.math.BigInteger;
 
 public class Lego_Blocks {
     public static void main(String[] args) {
-        lego(new int[][] {{10, 10},});
+        lego(new int[][] {{2, 2}, {3, 2}, {2, 3}, {4, 4}, {1, 10}, {10, 10}, {20, 20}, {5, 5}});
+        lego(new int[][] {{1000, 1000}});
     }
     public static void lego(int[][] arr) {
-        int width;
-        double height;
-        double[] P, C, N;
+        final int modulus = 1000000007;
+        final BigInteger modulusBitInt = new BigInteger("1000000007");
+        int width, height;
+        long[] P, N;
+        BigInteger[] oneLayer;
         for (int k = 0; k < arr.length; ++k) {
+            if (arr[k][0] <= 0 || arr[k][1] <= 0) {
+                System.out.println(0);
+                continue;
+            } else if (arr[k][0] == 1) {
+                System.out.println((arr[k][1] <= 4) ? 1 : 0);
+                continue;
+            }
             height = arr[k][0];
             width = arr[k][1];
-            P = new double[width + 1];
-            C = new double[width + 1];
-            N = new double[width + 1];
-            // Pre-processing number of ways to build 1 * 1 * width. DP-based. 
-            P[0] = 1;
+            P = new long[width + 1];
+            N = new long[width + 1];
+            oneLayer = new BigInteger[width + 1];
+            oneLayer[0] = new BigInteger("1");
             int i;
+            for (i = 1; i <= width; ++i) {
+                oneLayer[i] = new BigInteger("0");
+            }
+            // Pre-processing number of ways to build 1 * 1 * width. DP-based.
             for (i = 1; i < P.length; ++i) {
                 for (int prevBlock = 1; prevBlock <= 4; ++prevBlock) {
                     if (i - prevBlock >= 0) {
-                        P[i] += P[i - prevBlock];
+                        oneLayer[i] = oneLayer[i].add(oneLayer[i - prevBlock]);
                     } else {
                         break;
                     }
                 }
             }
-            for (i = 0; i <= width; ++i) {
-                P[i] = Math.pow(P[i], height);
+            for (i = 1; i < P.length; ++i) {
+                P[i] = pow(oneLayer[i].mod(modulusBitInt).intValue(), height, modulus);
+                N[i] = P[i];
             }
             // Calculate N. DB-based. 
             for (i = 1; i < P.length; ++i) {
-                for (int j = 1; j <= i - 1; ++j) {
-                    C[i] += P[j] * N[i - j];
+                for (int j = 1; j < i; ++j) {
+                    N[i] -= ((N[j] * (P[i - j])) % modulus);
+                    if (N[i] < 0) {
+                        N[i] += modulus;
+                    }
                 }
-                N[i] = P[i] - C[i];
             }
-            print(P);
-            print(C);
-            print(N);
-            System.out.println((int)(N[N.length - 1] % 1000000007));
+            System.out.println(N[N.length - 1]);
         }
     }
-    public static void print(double[] arr) {
-        for (double i : arr) System.out.print(i + " ");
-        System.out.println();
-    }
-}
-
-
-class Count {
-    public int remainder;
-    public int result;
-    Count(int x) {
-        this.remainder = x % 1000000007;
-        this.result = x / 1000000007;
-    }
-    public Count multiply(Count obj) {
-        Count ret = new Count(0);
-        ret.result = this.result * obj.result * 1000000007 + this.result * obj.remainder + this.remainder * obj.result + (this.remainder + obj.remainder) / 1000000007;
-        ret.remainder = (this.remainder + obj.remainder) % 1000000007;
-        return ret;
-    }
-    public Count add(Count obj) {
-        Count ret = new Count(0);
-        ret.result = this.result + obj.result + (this.remainder + obj.remainder) / 1000000007;
-        ret.remainder = (this.remainder + obj.remainder) % 1000000007;
-        return ret;
+    public static long pow(long base, int height, int modulus) {
+        // Time complexity O(log n).
+        long result = 1;
+        while (height > 0) {
+            if ((height & 0x1) == 1) {
+                // (result * tmp) % modulus = ((result % modulus) * (tmp & modulus)) % modulus
+                result *= base;
+                result = (result % modulus);
+            }
+            base *= base;
+            base = base % modulus;
+            height = (height >> 1);
+        }
+        return result;
     }
 }
