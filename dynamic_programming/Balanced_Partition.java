fc1ee322cdaf4f49d30504fff0b1a9ca2ab4685b
monkeylyf <laituan1986@gmail.com>
2013-03-24 20:43:59 -0700

    revised Balanced_Partition

--- dynamic_programming/Balanced_Partition.java
+++ dynamic_programming/Balanced_Partition.java
@@ -11,25 +11,26 @@ class Balanced_Partition {
     
     }
     public static void partition(int[] arr) {
+        // The problem is is sum / 2 is too large, say then million,
+        // we need sum / 2 * arr.length space. TODO: opt for space complexity?
         // If sum is even, each set should sum up to N/2
         // if sum is odd, one set should sum up to N/2 and another N/2 + 1.
         int sum = 0;
-        for (int i  : arr) {
-            sum += i;
+        for (int i : arr) {
+            sum += i; // sum of all element.
         }
-        boolean[][] status = new boolean[sum / 2 + 1][arr.length + 1];
+        boolean[][] status = new boolean[sum / 2 + 1][arr.length];
         for (int i = 0; i < arr.length; ++i) {
             status[0][i] = true;
         }
         for (int i = 1; i <= sum / 2; ++i) {
-            for (int j = 1; j <= arr.length; ++j) {
-                // if status[i][j - 1] == true, then status[i][j] == true.
-                // else status[i][j] == status[i - arr[j]][j - 1]
-                if (status[i][j - 1]) {
-                    status[i][j] = true;
-                } else {
-                    status[i][j] = (i - arr[j - 1] >= 0) ? status[i - arr[j - 1]][j - 1] : false;
-                }
+            status[i][0] = arr[0] == i; // if a0 can sum up to i only when a0 = i.
+            for (int j = 1; j < arr.length; ++j) {
+                // Check if (a, a1..aj-1) can sum up to i - j (i - j >= 0).
+                boolean oneStepBack = (i - arr[j] >= 0) ? status[i - arr[j]][j - 1] : false;
+                // if (a0,a1..aj-1) can sum up to i, then (a0,a1..aj) can sum up to i too.
+                // Use OR operation to find out if (a, a1..aj) can sum up to i
+                status[i][j] = oneStepBack || status[i][j - 1];
             }
         }
         printMatrix(status);
@@ -37,7 +38,11 @@ class Balanced_Partition {
     public static void printMatrix(boolean[][] status) {
         for (int i = 0; i < status.length; ++i) {
             for (int j = 0; j < status[i].length; ++j) {
-                System.out.print(status[i][j] + " ");
+                if (status[i][j]) {
+                    System.out.print("T ");
+                } else {
+                    System.out.print("F ");
+                }
             }
             System.out.println();
         }
