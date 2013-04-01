3f29ff9371bf219ba51bcfecac349b02abcf4133
monkeylyf <laituan1986@gmail.com>
2013-04-01 12:10:44 -0700

    revised google_Substring_With_Two_Distinct_Chars.java

--- str/google_Substring_With_Two_Distinct_Chars.java
+++ str/google_Substring_With_Two_Distinct_Chars.java
@@ -5,6 +5,11 @@ Given a string, find the longest substring with only two distinct characters.
 
 For exmaple,
 Given string "abbaacccaaabba", return "aacccaaa"
+
+
+You need to ask the interviewee about some edge cases you can think of like
+"", which is an empty string input and "a" which contains only one distinct
+char.
 */
 
 
@@ -22,33 +27,35 @@ public class Substring_With_Two_Distinct_Chars {
         System.out.println(solve("abbaacccaaabba").equals("aacccaaa"));
         System.out.println(solve("").equals(""));
         System.out.println(solve("a").equals(""));
+        System.out.println(solve("abbaabbaa").equals("abbaabbaa"));
     }
-    
+
     public static String solve(String str) {
         String ret = "";
-        int head = 0, tail = 0;
+        int head = 0, tail = 0, start = 0;
         HashSet<Character> set = new HashSet<Character>();
         while (tail < str.length()) {
             if (set.contains(str.charAt(tail))) {
                 ++tail;
             } else if (set.size() < 2) {
                 set.add(str.charAt(tail));
+                head = tail;
                 ++tail;
             } else {
                 // update ret.
-                ret = (tail - head > ret.length()) ? str.substring(head, tail) : ret;
-                // Retrospect to longest subarray with one distinct char.
-                tail -= 1;
-                head = tail;
-                while (str.charAt(head - 1) == str.charAt(tail)) {
-                    --head;
-                }
+                ret = (tail - start > ret.length()) ? str.substring(start, tail) : ret;
+                // Retrospect to longest subarray with one distinct char backwards.
                 tail = head;
+                // Reset start for substring starting point mark.
+                start = head;
                 // Clear set.
                 set.clear();
-                set.add(str.charAt(tail));
             }
         }
-        return ret;
+        if (set.size() != 2) { // For edge case like "a"
+            return "";
+        } else {
+            return (tail - start > ret.length()) ? str.substring(start, tail) : ret;
+        }
     }
 }
