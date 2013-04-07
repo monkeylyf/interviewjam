8b2cf3540a0bbe01f2f3de1633c314394c90c8c5
monkeylyf <laituan1986@gmail.com>
2013-04-06 17:14:27 -0700

    revised ../arr/leetcode_Merge_Sorted_Array.java

--- arr/leetcode_Merge_Sorted_Array.java
+++ arr/leetcode_Merge_Sorted_Array.java
@@ -11,21 +11,12 @@ class leetcode_Merge_Sorted_Array {
         merge(new int[] {2, 0}, 1, new int[] {1}, 1);
     }
     public static void merge(int A[], int m, int B[], int n) {
-        int a_tail = m - 1;
-        int b_tail = n - 1;
-        while (a_tail >= 0 && b_tail >= 0) {
-            if (A[a_tail] > B[b_tail]) {
-                A[a_tail + b_tail + 1] = A[a_tail];
-                --a_tail;
-            } else {
-                A[a_tail + b_tail + 1] = B[b_tail];
-                --b_tail
-            }
+        while (n > 0 && m > 0) {
+            if (A[m - 1] > B[n - 1]) A[m + n - 1] = A[m-- - 1];
+            else A[m + n - 1] = B[n-- - 1];
         }
         // The goal is to merge B into A. If B is done, merging is done.
         // Sanity check for if B is done.
-        while (b_tail >= 0) {
-            A[b_tail] = B[b_tail--];
-        }
+        while (n > 0) A[n - 1] = B[n-- - 1];
     }
 }
