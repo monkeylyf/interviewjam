d72dffb404fff5c325fb3996096f39959a97a01c
monkeylyf <laituan1986@gmail.com>
2013-04-06 17:53:35 -0700

    revised leetcode_Anagrams.java

--- str/leetcode_Anagrams.java
+++ str/leetcode_Anagrams.java
@@ -6,45 +6,34 @@ Note: All inputs will be in lower-case.
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.HashMap;
 
 class leetcode_Anagrams {
     public static void main(String[] args) {
         anagrams(new String[] {"tea","and","ace","ad","eat","dans"});
     }
     public static ArrayList<String> anagrams(String[] strs) {
-        ArrayList<String> all = new ArrayList<String>();
-        Hashtable<String, ArrayList<Integer>> dict = new Hashtable<String, ArrayList<Integer>>();
-        ArrayList<Integer> tmp;
-        for (int i = 0; i < strs.length; ++i) {
-            String sorted = Sort(strs[i]);
-            if (!dict.containsKey(sorted)) {
-                tmp = new ArrayList<Integer>();
-                tmp.add(i);
-                dict.put(sorted, tmp);
-            } else {
-                ArrayList<Integer> arr = dict.get(sorted);
-                arr.add(i);
-                dict.put(sorted, arr);
+        String anagram;
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        char[] arr;
+        ArrayList<String> tmp;
+        for (String i : strs) {
+            // Sort char array then converted to string to detect anagram.
+            arr = i.toCharArray();
+            Arrays.sort(arr);
+            anagram = new String(arr);
+            // Update cache.
+            tmp = (map.containsKey(anagram)) ? map.get(anagram) : new ArrayList<String>();
+            tmp.add(i);
+            map.put(anagram, tmp);
+        }
+        ArrayList<String> ret = new ArrayList<String>();
+        for (ArrayList<String> iter : map.values()) {
+            // Traverse cache to find anagram group who has more than one menber.
+            if (iter.size() > 1) {
+                ret.addAll(iter);
             }
         }
-        Set set = dict.keySet();
-        Iterator itr = set.iterator();
-        while (itr.hasNext()) {
-            String str = (String) itr.next();
-            if (dict.get(str).size() > 1) {
-                for (int i : dict.get(str)) {
-                    all.add(strs[i]);
-                }
-            }
-        }
-        return all;
-    }
-    public static String Sort(String a) {
-        char[] chars = a.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
+        return ret;
     }
 }
