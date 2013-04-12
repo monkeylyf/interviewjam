bba66a4c1c88ef435fab8f881eb38402f2febe3f
monkeylyf <laituan1986@gmail.com>
2013-04-12 12:50:55 -0700

    added Longest_Palindrome_Subsequence

---
+++ str/Longest_Palindrome_Subsequence.java
@@ -0,0 +1,42 @@
+/*Longest_Palindrome_Subsequence
+
+*/
+
+class Longest_Palindrome_Subsequence {
+    public static void main(String[] args) {
+        solve("BBABCBCAB");
+    }
+    
+    public static void solve(String str) {
+        int n = str.length(), L, i, j;
+        int[][] dp = new int[n][n];
+        char head, tail;
+        // init state. For single char, the longest palindrome is the char itself.
+        for (i = 0; i < n; ++i) {
+            dp[i][i] = 1;
+        }
+        for (L = 1; L <= n; ++L) { // down-to-top. L is the length of string.
+            for (j = 0; j < n - L; ++j) { // j is head index of string.
+                head = str.charAt(j);
+                tail = str.charAt(j + L);
+                //System.out.println("L " + L + " j " + j);
+                if (head == tail) {
+                    dp[j][j + L] = dp[j + 1][j + L - 1] + 2;
+                } else {
+                    dp[j][j + L] = Math.max(dp[j + 1][j + L], dp[j][j + L - 1]);
+                }
+            }
+        }
+        printMatrix(dp);
+    }
+    
+    public static void printArray(int[] arr) {
+        for (int i : arr) System.out.print(i + " ");
+        System.out.println();
+    }
+    
+    public static void printMatrix(int[][] m) {
+        for (int[] i : m) printArray(i);
+        System.out.println();
+    }
+}
