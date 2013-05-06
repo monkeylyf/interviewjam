9b894d9791949c3e51ba3e7e218ad5249539f566
monkeylyf <laituan1986@gmail.com>
2013-05-06 16:31:33 -0700

    added leetcode_Distinct_Subsequences.java

--- dynamic_programming/leetcode_Distinct_Subsequences.java
+++ dynamic_programming/leetcode_Distinct_Subsequences.java
@@ -1,4 +1,5 @@
 /*Distinct_Subsequences
+leetcode
 
 Given a string S and a string T, count the number of distinct subsequences of
 T in S.
@@ -9,29 +10,69 @@ relative positions of the remaining characters. (ie, "ACE" is a subsequence of
 Here is an example:
 S = "rabbbit", T = "rabbit"
 Return 3.
+
+Explanation:
+The body of this question confuses me a lot.
+Count how many distinct subsequence of S is equal to T.
+S has such subsequence "rabb_it", "rab_bit" and "ra_bbit" which equals to T.
 */
 
 class leetcode_Distinct_Subsequences {
     public static void main(String[] args) {
-        numDistinct("ccc", "c");
+        //numDistinct("ccc", "c");
+	//numDistinct("rabbbit", "rabbit");
+	num("rabbbit", "rabbit");
     }
     public static int numDistinct(String S, String T) {
-        //C(S, T) = C(S.substr(0,len-1), T.substr(0,len-1)) + C(S.substr(0,len-1), T)
-        int[] prev = new int[T.length() + 1];
-        int[] next = new int[T.length() + 1];
-        prev[0] = 1;
-        for (int i = 0; i < S.length(); ++i) {
-            next[0] = 1;
-            for (int j = 0; j <= i && j < T.length(); ++j) {
+        //C(S, T) = C( S.substr(len-1), T.substr(len-1) ) +
+	//          C( S.substr(len-1), T )
+	// explanation:
+	// for each char, there are two states: use it or not.
+	// if last char of two string are equal, use it + not use it.
+	// if not equal, inherit dp state.
+	int i, j, t = T.length(), s = S.length();
+        int[] dp = new int[t];
+        for (i = 0; i < s; ++i) {
+            for (j = t - 1; j >= 0; --j) {
                 if (S.charAt(i) == T.charAt(j)) {
-                    next[j + 1] = prev[j + 1] + prev[j];
-                } else {
-                    next[j + 1] = prev[j + 1];
+		    if (j == 0) {
+			dp[0] = dp[0] + 1;
+		    } else {
+                    	dp[j] = dp[j] + dp[j - 1];
+		    }
                 }
             }
-            prev = next;
-            next = new int[T.length() + 1];
+	    print(dp);
         }
-        return prev[next.length - 1];
+        return dp[t - 1];
+    }
+
+    public static int num(String S, String T) {
+	// This way we need two dp state array to track but for loop logic is more clear.
+	// The method above use only one dp state array.
+	int i, j, t = T.length(), s = S.length();
+        int[] dp = new int[t], next;
+        for (i = 0; i < s; ++i) {
+	    next = new int[t];
+            for (j = 0; j <= i && j < t; ++j) {
+                if (S.charAt(i) == T.charAt(j)) {
+		    if (j == 0) {
+			next[0] = dp[0] + 1;
+		    } else {
+                    	next[j] = dp[j] + dp[j - 1];
+		    }
+                } else {
+		    next[j] = dp[j];
+		}
+            }
+	    dp = next;
+	    print(dp);
+        }
+        return dp[t - 1];
+    }
+
+    public static void print(int[] arr) {
+    	for (int i : arr) System.out.print(i + " ");
+	System.out.println();
     }
 }
