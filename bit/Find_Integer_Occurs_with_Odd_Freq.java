5147541be3838302c36a56961dab151124a20e13
monkeylyf <laituan1986@gmail.com>
2013-05-11 14:39:57 -0700

    added Find_Integer_Occurs_with_Odd_Freq.java

---
+++ bit/Find_Integer_Occurs_with_Odd_Freq.java
@@ -0,0 +1,21 @@
+/*Find_Integer_Occurs_with_Odd_Freq
+
+Example:
+Intput: [1, 2, 3, 4, 3, 2, 1]
+Output: 4
+*/
+
+
+class Find_Integer_Occurs_with_Odd_Freq {
+	public static void main(String[] args) {
+		solve(new int[] {1, 2, 3, 4, 3, 2, 1});	
+	}	
+	
+	public static void solve(int[] arr) {
+		int ret = 0;
+		for (int i : arr) {
+			ret = ret ^ i;	
+		}
+		System.out.println(ret);
+	}
+}
