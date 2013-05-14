bcb212e7c18f03bdc4dece9c950ed3e9d9c2e1be
monkeylyf <laituan1986@gmail.com>
2013-05-14 09:34:16 -0700

    added Kights_Tour.java

---
+++ recursion/Kights_Tour.java
@@ -0,0 +1,51 @@
+/*Kights_Tour
+
+http://en.wikipedia.org/wiki/Knight%27s_tour
+*/
+
+class Kights_Tour {
+
+	public static void main(String[] args) {
+		solve(8, 0, 0);
+		solve(8, 0, 4);
+	}
+	
+	public static void solve(int n, int x, int y) {
+		int[][] path = new int[n][n];
+		if (jump(path, 1, x, y, n)) {
+			printMatrix(path);
+		} else {
+			System.out.println("impossible");
+		}
+	}
+	
+	public static boolean jump(int[][] path, int idx, int x, int y, int n) {
+		if (idx == n * n + 1) {
+			return true; // index begins with 1. n * n is the last to fill. End with n^2 + 1.
+		} else if (x < 0 || x >= n || y < 0 || y >= n || path[x][y] != 0) {
+			return false; // Index out of boundary, Trim.
+		} else {
+			path[x][y] = idx;
+			int[][] shifts = new int[][] {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+			for (int[] shift : shifts) {
+				if (jump(path, idx + 1, x + shift[0], y + shift[1], n)) {
+					return true;
+				}
+			}
+			path[x][y] = 0; // Reset. Backtracking.
+			return false;
+		}
+	}
+	
+	// Helper function.
+	public static void print(int[] arr) {
+		for (int i : arr) System.out.print(i + " ");
+		System.out.println();
+	}
+	
+	public static void printMatrix(int[][] m) {
+		for (int[] i : m) print(i);
+		System.out.println();
+	}
+
+}
