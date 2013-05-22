87f81bee6f87748c583d71d7b4f68701954738e7
monkeylyf <laituan1986@gmail.com>
2013-05-21 22:02:47 -0700

    revised leetcode_Scramble_String.java

--- dynamic_programming/leetcode_Scramble_String.java
+++ dynamic_programming/leetcode_Scramble_String.java
@@ -45,15 +45,48 @@ string of s1.
 class leetcode_Scramble_String {
     public static void main(String[] args) {
         //System.out.println(isScramble("abcde", "bacdef"));
-        System.out.println(isScramble("great", "rgtae"));
+        System.out.println(isScramble("great", "rgtae") == isScramble1("great", "rgtae"));
     }
-    public static boolean isScramble(String s1, String s2) {
+
+	public static boolean isScramble(String s1, String s2) {
+		// This solution is dp-based.
+		// dp[i][j][k] represents if s1.substr(i, i + k) is scramble of s2.substr(j, j + k)
+		// Then dp[0][0][n] is what we want eventually.
+		// Down-to-top. Staring from i = j = n - 1. k = 1.
+		int n = s1.length(), i, j, k, l;
+		boolean[][][] dp = new boolean[n][n][n + 1];
+		for (i = n - 1; i >= 0; --i) {
+			for (j = n - 1; j >= 0; --j) {
+				for (k = 1; k <= n - Math.max(i, j); ++k) {
+					if (s1.substring(i, i + k).equals(s2.substring(j, j + k))) {
+						// substrings are equal => is scramble based on definition.
+						dp[i][j][k] = true;	
+					} else {
+						for (l = 1; l < k; ++l) {
+							// index l is a pivot, which parts the string into two substring.
+								// s1.substr(i,i+l) is scramble of s2.substr(j,j+l) and 
+								// s1.substr(i+l) is scramble of s2.substr(j+l)
+							if ((dp[i][j][l] && dp[i + l][j + l][k - l]) ||
+								// s1.substr(i,i+l) is scramble of s2.substr(j+k-l) and
+								// s1.substr(j+l) is cramble of s2.substr(j)
+								(dp[i][j + k - l][l] && dp[i + l][j][k - l]))	 {
+								dp[i][j][k]	= true;
+								break;
+							}
+						}	
+					}
+				}	
+			}	
+		}
+		return dp[0][0][n];
+	}
+    public static boolean isScramble1(String s1, String s2) {
         // The idea behind is swapping.
-        int n = s1.length();
+        int n = s1.length(), i;
         if (n == 1) {
             return s1.equals(s2);
         } else {
-            for (int i = 1; i <= s2.length() / 2; i++) {
+            for (i = 1; i <= s2.length() / 2; ++i) {
                 if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
                     isScramble(s1.substring(i), s2.substring(i))) {
                     return true;
