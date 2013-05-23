849cb4cda12799a60c6176fd4cfb5c45cdb528d2
monkeylyf <laituan1986@gmail.com>
2013-05-23 16:39:01 -0700

    revised google_Find_Duplicate.py

--- bit/google_Find_Duplicate.py
+++ bit/google_Find_Duplicate.py
@@ -8,19 +8,20 @@ def find_duplicate(arr):
     # Solution xor all elements in array and xor all number from [1, n]
     # Then all elements are xor'ed two times and duplicates are xor'ed
     # three times. the result itself is the duplicate element
-    ret = 0
-    for i in arr:
-        ret = ret ^ i
-    for i in range(1, len(arr)):
-        ret = ret ^ i
-    return ret 
+    from functools import reduce
+    ret = reduce(lambda x, y : x ^ y, arr)
+    dup = reduce(lambda x, y : x ^ y, range(1, len(arr)))
+    return ret ^ dup
 
 
 def run():
     arr = [1,2,3,4,5,5,6,7,8]
-    assert(find_duplicate(arr) == 5)
+    #assert(find_duplicate(arr) == 5)
+    print find_duplicate(arr)
     arr = [1,2,3,3]
-    assert(find_duplicate(arr) == 3)
+    #assert(find_duplicate(arr) == 3)
+    print find_duplicate(arr)
+     
 
 if __name__ == '__main__':
     run()
