0a2df7f37b8cd5008f4c7d044f0d5078babc56ed
monkeylyf <laituan1986@gmail.com>
2013-06-09 10:37:53 -0700

    rm test_27.java

--- tree/test_27.java
+++
@@ -1,9 +0,0 @@
-/*You are given a binary tree in which each node contains a value. Design an algorithm
-to print all paths which sum up to that value. Note that it can be any path in the tree
-- it does not have to start at the root*/
-
-class test_27 {
-	public static void main(String[] args) {
-	
-	}
-}
