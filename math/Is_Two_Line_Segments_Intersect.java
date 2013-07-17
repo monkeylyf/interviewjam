1ac47e59b32abbed4a321d9cb38e473e962a93d8
Yifeng Liu <laituan1986@gmail.com>
2013-07-17 09:47:51 -0700

    fixed a bug in Is_Two_Line_Segments_Intersect.java; added more test cases

--- math/Is_Two_Line_Segments_Intersect.java
+++ math/Is_Two_Line_Segments_Intersect.java
@@ -39,27 +39,42 @@ public class Is_Two_Line_Segments_Intersect {
 		Point p2 = new Point(1, 2);
 		Point q1 = new Point(10, 1);
 		Point q2 = new Point(10, 2);
-		System.out.println(isIntersected(p1, q1, p2, q2));
+		System.out.println(isIntersected(p1, q1, p2, q2)); // False
 		// test case 2. 
 		p1 = new Point(10, 0);
 		p2 = new Point(0, 0);
 		q1 = new Point(0, 10);
 		q2 = new Point(10, 10);
-		System.out.println(isIntersected(p1, q1, p2, q2));
+		System.out.println(isIntersected(p1, q1, p2, q2)); // True
 		// test case 3.
 		p1 = new Point(-5, -5);
 		p2 = new Point(1, 1);
 		q1 = new Point(0, 0);
 		q2 = new Point(10, 10);
-		System.out.println(isIntersected(p1, q1, p2, q2));
+		System.out.println(isIntersected(p1, q1, p2, q2)); // False
+		// test case 4.
+		p1 = new Point(10, 0);
+		p2 = new Point(5, 5);
+		q1 = new Point(10, 10);
+		q2 = new Point(1000000, 5);
+		System.out.println(isIntersected(p1, q1, p2, q2)); // True
+		// test case 5.
+		p1 = new Point(0, 0);
+		p2 = new Point(5, 5);
+		q1 = new Point(0, 10);
+		q2 = new Point(1000000, 5);
+		System.out.println(isIntersected(p1, q1, p2, q2)); // False
 	}
-	
+
+	// The main function that returns true if line segment 'p1q1'
+	// and 'p2q2' intersect.
 	public static boolean isIntersected(Point p1, Point q1, Point p2, Point q2) {
 		int o1 = orientation(p1, q1, p2);
 	    int o2 = orientation(p1, q1, q2);
 	    int o3 = orientation(p2, q2, p1);
 	    int o4 = orientation(p2, q2, q1);
-	    
+	   
+		//System.out.println(o1 + "|"+ o2 +"|"+ o3 +"|"+ o4);
 	    if (o1 != o2 && o3 != o4) {
 			// General case. If two sets of orientation are different
 			// they are intersected.
@@ -89,7 +104,7 @@ public class Is_Two_Line_Segments_Intersect {
 
 	// Return the orientation type of point p, q, r.
 	public static int orientation(Point p, Point q, Point r) {
-		int slope_diff = (p.y - q.y) * (r.x - r.x) - (q.y - r.y) * (p.x - q.x);
+		int slope_diff = (p.y - q.y) * (q.x - r.x) - (q.y - r.y) * (p.x - q.x);
 		if (slope_diff == 0) {
 			return COLINEAR;
 		} else if (slope_diff > 0) {
