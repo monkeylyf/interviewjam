1cbc988e8c0f0b44fb3f292b7dc7cd221d248186
Yifeng Liu <laituan1986@gmail.com>
2013-09-02 12:03:11 -0700

    modified leetcode_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java; tried to use Arrays.copyOfRange to make array copy more java but failed some test cases

--- tree/leetcode_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
+++ tree/leetcode_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -6,40 +6,40 @@ Note:
 You may assume that duplicates do not exist in the tree.
 */
 
+import java.util.*;
+
 
 class leetcode_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal {
+
     public static void main(String[] args) {
         buildTree(new int[] {1,3,2}, new int[] {3,2,1});
+        buildTree(new int[] {-1}, new int[] {-1});
     }
-    public static TreeNode buildTree(int[] inorder, int[] postorder) {
-        return nextNode(inorder, postorder);
-    }
-    public static TreeNode nextNode(int[] in, int[] post) {
+
+    public static TreeNode buildTree(int[] in, int[] post) {
         if (in.length != 0 && post.length != 0) {
-            TreeNode node = new TreeNode(post[post.length - 1]);
-            int index = 0;
-            for (int i = 0 ; i < in.length; ++i) {
-                if (in[i] == post[post.length - 1]) {
-                    index = i;
+            TreeNode root = new TreeNode(post[post.length - 1]);
+            int idx = 0;
+            for (; idx < in.length; ++idx) { // Find the index of root val.
+                if (in[idx] == root.val) {
+                    break;
                 }
             }
             // Create inorder & postorder for left child node.
-            int[] left_in = new int[index];
-            int[] left_post = new int[index];
-            for (int i = 0; i < index; ++i) {
+            int[] left_in = new int[idx], left_post = new int[idx];
+            for (int i = 0; i < idx; ++i) {
                 left_in[i] = in[i];
                 left_post[i] = post[i];
             }
             // Create inorder & postorder for right child node.
-            int[] right_in = new int[in.length - index - 1];
-            int[] right_post = new int[in.length - index - 1];
-            for (int i = index + 1; i < in.length; ++i) {
-                right_in[i - index - 1] = in[i];
-                right_post[i - index - 1] = post[i - 1];
+            int[] right_in = new int[in.length - idx - 1], right_post = new int[in.length - idx - 1];
+            for (int i = idx + 1; i < in.length; ++i) {
+                right_in[i - idx - 1] = in[i];
+                right_post[i - idx - 1] = post[i - 1];
             }
-            node.left = nextNode(left_in, left_post);
-            node.right = nextNode(right_in, right_post);
-            return node;
+            root.left = buildTree(left_in, left_post);
+            root.right = buildTree(right_in, right_post);
+            return root;
         } else {
             return null;
         }
@@ -51,7 +51,10 @@ class TreeNode {
     TreeNode left;
     TreeNode right;
     int val;
+
     TreeNode(int x) {
-        val = x;
+		this.left = null;
+		this.right = null;
+        this.val = x;
     }
 }
