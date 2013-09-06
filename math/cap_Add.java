23b67a15ff8dbfae7efbff72cd54bbc9a7dff8ea
Yifeng Liu <laituan1986@gmail.com>
2013-09-05 19:34:51 -0700

    modifed cap_Add.java; added more comments and and the recursion solution to non-recursion

--- math/cap_Add.java
+++ math/cap_Add.java
@@ -5,15 +5,28 @@ Write a function that adds two numbers You should not use + or any arithmetic
 operators
 */
 
-class cap_Add {
+public class cap_Add {
+
 	public static void main(String[] args) {
-		System.out.println(add(123, 789));
+		// Test case.
+		System.out.println(add(123, 789) == 912);
+		System.out.println(add(5, 7) == 12);
 	}
+
+	// Thought:
+	// decimal addition is equivalent to binary addition, so we can take advantage of
+	// binary operators given arithmetic operators prohibited.
 	public static int add(int a, int b) {
 		// The tricky part to how to approve that b will be 0 eventually?
-		if (b == 0) {
-            return a;
-        }
-		return add(a ^ b, (a & b) << 1);
+		// Answer:
+		// xor and and operation are mutually exclusive and so are their results.
+		// Shift by one will eventually elimiate more 1's in b and it will converge.
+		int xor;
+		while (b != 0) {
+			xor = a ^ b; // xor operation filters the different bit at the same position.
+			b = (a & b) << 1; // and operation filters the same bit at the same position. Shift by 1 to add them up.
+			a = xor;
+		}
+		return a;
 	}
 }
