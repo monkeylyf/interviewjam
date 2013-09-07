84ae779572a6a96ceb40f157c043b451f5086a60
Yifeng Liu <laituan1986@gmail.com>
2013-09-06 17:30:56 -0700

    modified cap_Search_a_Sorted_Matrix.java

--- searching/cap_Search_a_Sorted_Matrix.java
+++ searching/cap_Search_a_Sorted_Matrix.java
@@ -4,30 +4,44 @@ careercup
 Given a matrix in which each row and each column is sorted, write a method to
 find an element in it.
 
-Mark as duplicate:
-google_kth_Maximum_Element_in_Sorted_Matrix
+Please check:
+google_kth_Maximum_Element_in_Sorted_Matrix.
+
+These two questions are strongly-related.
 */
 
-class cap_Search_a_Sorted_Matrix {
+public class cap_Search_a_Sorted_Matrix {
+
 	public static void main(String[] args) {
-		int[][] m = {{0, 5, 10, 15},
-			     {1, 6, 11, 16},
-			     {2, 7, 12, 17},
-			     {3, 8, 13, 18},
-			     {4, 9, 14, 19}};
-		for (int i : findIndexOfMatrix(m, 18, 5, 4)) System.out.println(i);
-		for (int i : findIndexOfMatrix(m, 13, 5, 4)) System.out.println(i);
-		for (int i : findIndexOfMatrix(m, 9, 5, 4)) System.out.println(i);
-		for (int i : findIndexOfMatrix(m, -9, 5, 4)) System.out.println(i);
-	}
-	public static int[] findIndexOfMatrix(int[][] m, int element, int M, int N) {
-		int row = 0;
-		int col = N - 1;
-		while (row < M && col >= 0) {
-			if (m[row][col] == element) return new int[] {row, col};
-			else if (m[row][col] > element) --col;
-			else ++row;
+		// Test case.
+		int[][] m = {
+				 {1, 2, 6, 10},
+			     {3, 4, 7, 13},
+			     {5, 9, 11, 14},
+			     {8, 12,15, 17}
+				    };
+		for (int i = 0; i < 20; ++i) {
+			System.out.println(findIndexBinary(m, i));	
 		}
-		return new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE};
+	}
+
+	// Divide and conquer. This solution requires recursion, which is not preferred.
+
+	// Binary Search.
+	public static boolean findIndexBinary(int[][] m, int target) {
+		int row = m.length, col = m[0].length;
+		// Starting from upperright.
+		int rowCursor = 0, colCursor = col - 1, cur;
+		while (rowCursor < row && colCursor >= 0) {
+			cur = m[rowCursor][colCursor];
+			if (cur == target) {
+				return true;	
+			} else if (cur > target) {
+				--colCursor;
+			} else { // cur < target
+				++rowCursor;	
+			}
+		}
+		return false;
 	}
 }
