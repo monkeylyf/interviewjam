0bd1adc2ef6feeb6e6cd0a9596003deb53785f52
Yifeng Liu <laituan1986@gmail.com>
2013-09-08 10:49:38 -0700

    tweaked the Create_Mirror_of_Tree.java and added more comments

--- tree/Create_Mirror_of_Tree.java
+++ tree/Create_Mirror_of_Tree.java
@@ -1,24 +1,25 @@
 /*Create_Mirror_of_Tree
-
+geeksforgeeks
 */
 
 import java.util.*;
 
-class Create_Mirror_of_Tree {
+public class Create_Mirror_of_Tree {
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(1);
-	    root.left        = new TreeNode(2);
-	    root.right       = new TreeNode(3);
-	    root.left.left  = new TreeNode(4);
+	    root.left = new TreeNode(2);
+	    root.right = new TreeNode(3);
+	    root.left.left = new TreeNode(4);
 	    root.left.right = new TreeNode(5);
-	    root.right.left  = new TreeNode(6);
+	    root.right.left = new TreeNode(6);
 	    root.left.right.left = new TreeNode(7);
 	    root.left.right.right = new TreeNode(8);
 	    bfs(root);
 	    bfs(copy(root));
 	}
 
+	// dfs to copy. Right node copied to left, left node copied to right.
 	public static TreeNode copy(TreeNode root) {
 		if (root == null) {
 			return null;
@@ -32,11 +33,10 @@ class Create_Mirror_of_Tree {
 
 	// Helper function to print out tree layer by layer.
 	public static void bfs(TreeNode root) {
-		Queue<TreeNode> cur = new LinkedList<TreeNode>();
-		Queue<TreeNode> next = new LinkedList<TreeNode>();
-		
+		Queue<TreeNode> cur = new LinkedList<TreeNode>(), next = new LinkedList<TreeNode>();
 		cur.add(root);
 		TreeNode curNode;
+
 		while (!cur.isEmpty()) {
 			curNode = cur.poll();
 			System.out.print(curNode + " ");
@@ -70,6 +70,6 @@ class TreeNode {
 	}
 		
 	public String toString() {
-		return "<" + this.val + ">";
+		return String.format("<%d>", this.val);
 	}
 }
