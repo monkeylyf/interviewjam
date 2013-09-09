44afed0505a1101d6d87a0e87e18b0f650e2b463
Yifeng Liu <laituan1986@gmail.com>
2013-09-09 09:35:00 -0700

    added Max_Product_Cutting.java

---
+++ dynamic_programming/Max_Product_Cutting.java
@@ -0,0 +1,49 @@
+/*Max_Product_Cutting
+
+
+Given a rope of length n meters, cut the rope in different parts of integer
+lengths in a way that maximizes product of lengths of all parts. You must make
+at least one cut. Assume that the length of rope is more than 2 meters.
+*/
+
+public class Max_Product_Cutting {
+	
+	public static void main(String[] args) {
+		System.out.println(solve(2));
+		System.out.println(solve(3));
+		System.out.println(solve(4));
+		System.out.println(solve(5));
+		System.out.println(solve(10));
+	}
+
+	public static int solve(int len) {
+		assert(len >= 2);
+		// - - - - - - a rope of length 6.
+		//  1 2 3 4 5 6
+		//
+		int i, j, localMax;
+		// Init dp state.
+		int[] dp = new int[len + 1];
+		dp[1] = 1;
+		// dp.
+		for (i = 2; i <= len; ++i) {
+			localMax = Integer.MIN_VALUE;
+			for (j = 1; j <= i / 2; ++j) {
+				localMax = Math.max(localMax, dp[i - j] * j); // Two cuts, one between j and i-j and another within i-j 
+				localMax = Math.max(localMax, j * (i - j)); // One cut between j and i-j.
+			}
+			dp[i] = localMax;
+		}
+		print(dp);
+		return dp[len];
+	}
+
+	// Helper function.
+	public static int max(int[] arr, int i, int j) {
+		int ret = Integer.MIN_VALUE;
+		for (; i <=j; ++i) {
+			ret = (ret < arr[i]) ? arr[i] : ret;	
+		}
+		return ret;
+	}
+}
