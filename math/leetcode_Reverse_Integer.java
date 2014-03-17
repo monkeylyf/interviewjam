90244594bba7babcc561127f7281e7f9a5cf014d
Yifeng Liu <laituan1986@gmail.com>
2014-03-17 10:05:03 -0700

    added python version to leetcode_Reverse_Integer.java

--- math/leetcode_Reverse_Integer.java
+++ math/leetcode_Reverse_Integer.java
@@ -3,6 +3,13 @@
 Reverse digits of an integer.
 Example1: x = 123, return 321
 Example2: x = -123, return -321
+
+
+Questions you should ask your interviewer:
+1. last digit is 0: 10, 100
+How to reverse? 01 or 001 or just 1?
+2. int overflow
+Assuming 32-bit integer, reverse of 100000003 will cause overflows
 */
 
 public class leetcode_Reverse_Integer {
@@ -26,3 +33,24 @@ public class leetcode_Reverse_Integer {
         return ret;
     }
 }
+
+/*Python Version
+Using str to reverse. yeah it's cheating.
+
+def reverse(self, x):
+    neg = False if x > 0 else True
+    x = abs(x)
+    x = int(''.join(reversed(str(x))))
+	return -x if neg else x
+
+def reverse(self, x):
+    neg = False if x > 0 else True
+    x = abs(x)
+    ret = 0
+    
+    while x:
+        ret *= 10
+        ret += x % 10
+        x /= 10
+    return -ret if neg else ret
+*/
