eb6e1244bcd494bae2ec369f68b587e02f0418a4
Yifeng Liu <laituan1986@gmail.com>
2014-03-22 22:28:22 -0700

    added python version to leetcode_Best_Time_to_Buy_and_Sell_Stock_III.java

--- arr/leetcode_Best_Time_to_Buy_and_Sell_Stock_III.java
+++ arr/leetcode_Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -12,38 +12,67 @@ the stock before you buy again).
 import java.util.ArrayList;
 
 
-class Best_Time_to_Buy_and_Sell_Stock_III {
+public class leetcode_Best_Time_to_Buy_and_Sell_Stock_III {
     public static void main(String[] args) {
-        maxProfit(new int[] {1,2,4,2,5,7,2,4,9,0});
+        //maxProfit(new int[] {1,2,4,2,5,7,2,4,9,0});
+		maxProfit(new int[] {2, 1, 2, 0, 1});
     }
-    public static int maxProfit(int[] prices) {
-        if (prices.length ==0) {
+
+    public static int maxProfit(int[] prices) { 
+        if (prices.length == 0) {
             return 0;
         }
         int retval = 0;
         int[] premax = new int[prices.length];
         int[] postmax = new int[prices.length];
-        //premax[0] = 0;
-        //postmax[prices.length - 1] = 0;
         int low = prices[0];
         int high = prices[prices.length - 1];
+
         for (int m = 1; m < prices.length; m++) {
             premax[m] = Math.max(premax[m - 1], prices[m] - low);
             low = Math.min(low, prices[m]);
         }
+
         for (int n = prices.length - 2; n >= 0; n--) {
             postmax[n] = Math.max(postmax[n + 1], high - prices[n]);
             high = Math.max(high, prices[n]);
         }
-        print(premax);
-        print(postmax);
+
         for (int j = 1; j < prices.length; j++) {
-            retval = Math.max(retval, (premax[j] + postmax[j]));
+            retval = Math.max(retval, premax[j] + postmax[j]);
         }
+		System.out.println(retval);
         return retval;
     }
+
     public static void print(int[] A) {
         for (int i : A) System.out.print(i + " ");
         System.out.println();
     }
 }
+
+
+/* Python Version
+def maxProfit(self, prices):
+    if not prices:
+        return 0
+    
+    ret = 0
+    forward = [ 0 for _ in xrange(len(prices)) ]
+    backward = [ 0 for _ in xrange(len(prices)) ]
+    
+    local_min = prices[0]
+    for i in xrange(1, len(prices)):
+        forward[i] = max(forward[i - 1], prices[i] - local_min)
+        local_min = min(local_min, prices[i])
+    
+    local_max = prices[-1]
+    for i in reversed(xrange(len(prices) - 1)):
+        backward[i] = max(backward[i + 1], local_max - prices[i])
+        local_max = max(local_max, prices[i])
+        
+    for i in xrange(1, len(prices)):
+        ret = max(ret, forward[i] + backward[i])
+        
+    return ret
+*/
