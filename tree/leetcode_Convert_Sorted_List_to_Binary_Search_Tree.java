e117a09ff911a7330a6e9fb4db16487cf744dc03
Yifeng Liu <laituan1986@gmail.com>
2014-03-24 20:25:21 -0700

    previous solution uses extra O(n) space to put linked list into array which has random access. it is cheating; added python version to it but did not pass OJ

--- tree/leetcode_Convert_Sorted_List_to_Binary_Search_Tree.java
+++ tree/leetcode_Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -6,56 +6,93 @@ convert it to a height balanced BST.
 
 public class leetcode_Convert_Sorted_List_to_Binary_Search_Tree {
 
-    public static void main(String[] args) {
-    
-    }
+  public static void main(String[] args) {
 
-    // Convert linked list to int array.
-    public static TreeNode sortedListToBST(ListNode head) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        ListNode node = head; 
-        while (node != null) {
-            arr.add(node.val);
-            node = node.next;
-        }
-        int[] num = new int[arr.size()];
-        for (int i = 0; i < arr.size(); ++i) {
-            num[i] = arr.get(i);
-        } 
-        return nextNode(num, 0, num.length - 1);
-    }
+  }
 
-    public static TreeNode nextNode(int[] num, int head, int tail) {
-        if (head <= tail) {
-            int middle = head + (tail - head) / 2;
-            TreeNode node = new TreeNode(num[middle]);
-            node.left = nextNode(num, head, middle - 1);
-            node.right = nextNode(num, middle + 1, tail);
-            return node;
-        } else {
-            return null;
-        }
+  public static TreeNode sortedListToBST(ListNode head) {
+	if (head == null) {
+	  return null;
+	}
+	if (head.next == null) {
+	  return new TreeNode(head.val);
+	}
+
+	ListNode dummy = new ListNode(-1);
+	dummy.next = head;
+	ListNode prev = dummy;
+	ListNode mid = head;
+	ListNode tail = head;
+
+	while (tail != null && tail.next != null) {
+	  prev = prev.next;
+	  mid = mid.next;
+	  tail = tail.next.next;
+	}
+
+	ListNode right = mid.next;
+	mid.next = null;
+	prev.next = null;
+	TreeNode ret = new TreeNode(mid.val);
+	ret.left = sortedListToBST(head);
+	ret.right = sortedListToBST(right);
+
+	return ret;
+  }
 }
 
 
 class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
+  int val;
+  ListNode next;
+  ListNode(int x) {
+	val = x;
+	next = null;
+  }
 }
 
 
 class TreeNode {
-    TreeNode left;
-    TreeNode right;
-    int val;
+  TreeNode left;
+  TreeNode right;
+  int val;
 
-    TreeNode(int val) {
-		this.left = null;
-		this.right = null;
-		this.val = val;
-	}
+  TreeNode(int val) {
+	this.left = null;
+	this.right = null;
+	this.val = val;
+  }
 }
+
+
+/* Python Version (Did not pass OJ, do not know why:()
+class Solution:
+    # @param head, a list node
+    # @return a tree node
+    def sortedListToBST(self, head):
+        if not head:
+            return None
+        
+        if not head.next:
+            return TreeNode(head.val)
+        
+        #
+        dummy = ListNode(-1)
+        dummy.next = head
+        
+        prev = dummy
+        slow = head
+        fast = head
+        while fast and fast.next:
+            prev = prev.next
+            slow = slow.next
+            fast = fast.next.next
+        
+        cur = ListNode(slow.val)
+        prev.next = None
+        right = slow.next
+        slow.next = None
+        cur.left = self.sortedListToBST(head)
+        cur.right = self.sortedListToBST(right)
+        return cur
+*/
