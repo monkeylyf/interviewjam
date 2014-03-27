0959f42f69d4bb32684cbbab939738ee86f958e2
Yifeng Liu <laituan1986@gmail.com>
2014-03-27 16:28:40 -0700

    added python version to leetcode_Decode_Ways.java

--- dynamic_programming/leetcode_Decode_Ways.java
+++ dynamic_programming/leetcode_Decode_Ways.java
@@ -58,3 +58,72 @@ class leetcode_Decode_Ways {
         return c1;
     }
 }
+
+/* Python Version
+Could use constant space instead of O(n) like Java code above.
+
+def numDecodings(self, s):
+    if not s:
+        return 0
+
+    if s[0] == '0':
+        return 0
+    
+    c0 = c1 = 1
+    for i in xrange(1, len(s)):
+        one = int(s[i])
+        two = int(s[i - 1 : i + 1])
+        
+        useOne = one != 0
+        useTwo = two >= 10 and two <= 26
+        
+        if useOne and useTwo:
+            c1 += c0
+            c0 = c1 - c0
+        elif useOne and not useTwo:
+            c0 = c1
+        elif not useOne and useTwo:
+            swap = c1
+            c1 = c0
+            c0 = c1
+        else:
+            return 0
+    return c1
+*/
+
+/* Python Version
+O(n) space. Not really elagent since you have to take care dp[1], which is quite messy.
+
+def numDecodings(self, s):
+    if not s:
+        return 0
+    
+    if s[0] == '0':
+        return 0
+    
+    
+    dp = [ 0 for _ in xrange(len(s)) ]
+    dp[0] = 1
+    if len(s) == 1:
+        return dp[0]
+    if s[1] == '0':
+        dp[1] = 1 if s[0] in ('1', '2') else 0
+    else:
+        dp[1] = 2 if int(s[:2]) <= 26 else 1
+    for i in xrange(2, len(s)):
+        one = int(s[i])
+        two = int(s[i - 1 : i + 1])
+        useOne = one != 0
+        useTwo = two >= 10 and two <= 26
+
+        if useOne and useTwo:
+            dp[i] = dp[i - 1] + dp[i - 2]
+        elif useOne and not useTwo:
+            dp[i] = dp[i - 1]
+        elif not useOne and useTwo:
+            dp[i] = dp[i - 2]
+        else:
+            return 0
+    
+    return dp[-1]
+*/
