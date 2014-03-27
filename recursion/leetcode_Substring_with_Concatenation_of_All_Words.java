01bd323d5346cc7df3ae70ebc0ef41179bfa71c1
Yifeng Liu <laituan1986@gmail.com>
2014-03-27 14:40:25 -0700

    added python version to leetcode_Substring_with_Concatenation_of_All_Words.java

--- recursion/leetcode_Substring_with_Concatenation_of_All_Words.java
+++ recursion/leetcode_Substring_with_Concatenation_of_All_Words.java
@@ -56,3 +56,58 @@ public class leetcode_Substring_with_Concatenation_of_All_Words {
         return res;
     }
 }
+
+
+/* Python Version
+Ok, couple of notes here.
+1. O(len(S) * len)
+2. L might contain the mutilple same items in it, like ['a', 'b', 'a'] so hashset won't work.
+3. Given 2, key/value pair(dict) is create to track the # of appearance, However, leetcode
+   clearly does not support 'from copy import deepcopy' and reset the freq is really neccesary.
+   Looks like manually copy is faster than recreate the dict
+
+
+class Solution:
+    # @param S, a string
+    # @param L, a list of string
+    # @return a list of integer
+    def findSubstring(self, S, L):
+        ret = []
+        c = self.count(L)
+        used = self.copy(c)
+        i = 0
+        length = len(L[0])
+
+        while i <= len(S) - len(L) * length:
+            for j in xrange(len(L)):
+                token = S[i + j * length : i + (j + 1) * length ]
+                if token in used and used[token] > 0:
+                    used[token] -= 1
+                else:
+                    break
+
+            #print used.values()
+            if not any(used.values()):
+                ret.append(i)
+                i += length
+            else:
+                i += 1
+            used = self.copy(c)
+
+        return ret
+    
+    def copy(self, d):
+        ret = {}
+        for key, value in d.iteritems():
+            ret[key] = value
+        return ret
+    
+    def count(self, L):
+        used = {}
+        for item in L:
+            try:
+                used[item] += 1
+            except KeyError:
+                used[item] = 1
+        return used
+*/
