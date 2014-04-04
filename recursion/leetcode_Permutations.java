fd82fc931c266e6ea5e081d6554724e8e7542bd5
Yifeng Liu <laituan1986@gmail.com>
2014-04-04 15:13:09 -0700

    added python version to leetcode_Permutations.java

--- recursion/leetcode_Permutations.java
+++ recursion/leetcode_Permutations.java
@@ -11,43 +11,63 @@ import java.util.ArrayList;
 
 public class leetcode_Permutations {
 
-    public static void main(String[] args) {
-		// Test case 1.
-        for (ArrayList<Integer> i : permute(new int[] {0, 1})) {
-			System.out.println(i);
-		}
-		// Test case 2.
-        for (ArrayList<Integer> i : permute(new int[] {0, -1, 1})) {
-			System.out.println(i);
-		}
-    }
+  public static void main(String[] args) {
+	// Test case 1.
+	for (ArrayList<Integer> i : permute(new int[] {0, 1})) {
+	  System.out.println(i);
+	}
+	// Test case 2.
+	for (ArrayList<Integer> i : permute(new int[] {0, -1, 1})) {
+	  System.out.println(i);
+	}
+  }
 
-    public static ArrayList<ArrayList<Integer>> permute(int[] num) {
-        ArrayList<ArrayList<Integer>> all = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> tmp = new ArrayList<Integer>();
-        boolean[] used = new boolean[num.length]; // boolean[] to track used int.
-        nextNum(num, used, tmp, all);
-        return all;
-    }
+  public static ArrayList<ArrayList<Integer>> permute(int[] num) {
+	ArrayList<ArrayList<Integer>> all = new ArrayList<ArrayList<Integer>>();
+	ArrayList<Integer> tmp = new ArrayList<Integer>();
+	boolean[] used = new boolean[num.length]; // boolean[] to track used int.
+	nextNum(num, used, tmp, all);
+	return all;
+  }
 
-    public static void nextNum(int[] num, boolean[] used, ArrayList<Integer> tmp, ArrayList<ArrayList<Integer>> all) {
-        if (tmp.size() == num.length) {
-            ArrayList<Integer> res = new ArrayList<Integer>();
-            for (int i : tmp) {
-                res.add(num[i]);
-            }
-            all.add(res);
-        } else {
-            for (int i = 0; i < num.length; ++i) {
-                if (!used[i]) {
-                    tmp.add(i);
-                    used[i] = true;
-                    nextNum(num, used, tmp, all);
-					// Backtracking reset.
-                    tmp.remove(tmp.size() - 1);
-                    used[i] = false;
-                }
-            }
-        }
-    }
+  public static void nextNum(int[] num, boolean[] used, ArrayList<Integer> tmp, ArrayList<ArrayList<Integer>> all) {
+	if (tmp.size() == num.length) {
+	  ArrayList<Integer> res = new ArrayList<Integer>();
+	  for (int i : tmp) {
+		res.add(num[i]);
+	  }
+	  all.add(res);
+	} else {
+	  for (int i = 0; i < num.length; ++i) {
+		if (!used[i]) {
+		  tmp.add(i);
+		  used[i] = true;
+		  nextNum(num, used, tmp, all);
+		  // Backtracking reset.
+		  tmp.remove(tmp.size() - 1);
+		  used[i] = false;
+		}
+	  }
+	}
+  }
 }
+
+/* Python Version
+def permute(self, num):
+    def dfs(num, container, acc, visited):
+        if len(acc) == len(num):
+            container.append(acc[::])
+        else:
+            for i, val in enumerate(num):
+                if not visited[i]:
+                    visited[i] = True
+                    acc.append(val)
+                    dfs(num, container, acc, visited)
+                    visited[i] = False
+                    acc.pop()
+        
+    visited = [ False ] * len(num)
+    container = []
+    dfs(num, container, [], visited)
+    return container
+*/
