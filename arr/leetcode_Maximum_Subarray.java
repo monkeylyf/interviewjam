a310e4dc007a7f59087492315413492fc9c321d7
Yifeng Liu <laituan1986@gmail.com>
2014-04-04 10:53:34 -0700

    added python version to leetcode_Maximum_Subarray.java

--- arr/leetcode_Maximum_Subarray.java
+++ arr/leetcode_Maximum_Subarray.java
@@ -11,31 +11,43 @@ the divide and conquer approach, which is more subtle.
 
 
 class leetcode_Maximum_Subarray {
-    public static void main(String[] args) {
-    }
-    public static int maxSubArray(int[] A) {
-        boolean noPositive = true;
-        int sum = 0;
-        int max = 0;
-        int localMax = Integer.MIN_VALUE;
-        for (int i = 0; i < A.length; ++i) {
-            if (A[i] > 0 && noPositive) {
-                // Flag if true indicating all elements <= 0.
-                noPositive = false;
-            }
-            localMax = Math.max(localMax, A[i]);
-            sum += A[i];
-            if (sum > max) {
-                max = sum;
-            } else if (sum < 0) {
-                sum = 0;
-            }
-        }
-        if (noPositive) {
-            // if all element are negtive, return the maximum element.
-            return localMax;
-        } else {
-            return max;
-        }
-    }
+
+  public static void main(String[] args) {
+	System.out.println(maxSubArray(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+  }
+
+  public static int maxSubArray(int[] A) {
+	boolean allNeg = true; // All elem are neg is special case.
+	int sum = 0;
+	int max = 0;
+	int localMax = Integer.MIN_VALUE;
+	for (int i = 0; i < A.length; ++i) {
+	  allNeg = (A[i] > 0) ? false : allNeg;
+	  localMax = Math.max(localMax, A[i]);
+	  sum += A[i];
+	  if (sum < 0) {
+		sum = 0;
+	  }
+	  max = Math.max(max, sum);
+	}
+
+	return (allNeg) ? localMax : max;
+  }
 }
+
+/* Python Version
+def maxSubArray(self, A):
+    if not A:
+        return 0
+    
+    all_neg = True
+    max_val = 0
+    acc = 0
+    for val in A:
+        all_neg = False if val > 0 else all_neg
+        acc += val
+        if acc < 0:
+            acc = 0
+        max_val = max(max_val, acc)
+    return max_val if not all_neg else max(A)
+*/
