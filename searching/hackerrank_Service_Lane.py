8d205e13547906ac1a8ba1205d984b141cd12e82
Yifeng Liu <laituan1986@gmail.com>
2014-04-05 15:36:24 -0700

    modified hackerrank_Service_Lane.py and put some comments on seg tree

--- searching/hackerrank_Service_Lane.py
+++ searching/hackerrank_Service_Lane.py
@@ -12,41 +12,54 @@ class SegTree(object):
         self.lane = lane
         self.length = len(lane)
         self.tree = None
-        self.build_tree(lane)
+        self.build_tree()
        
-    def build_tree(self, lane):
+    def build_tree(self):
+        """Build Segment Tree."""
+        # Calc height and size of the tree.
         height = int(math.ceil(math.log(self.length, 2)))
         size = 2 * int(pow(2, height)) - 1
-        self.tree = [ 0 for _ in xrange(size) ]
+        self.tree = [ 0 ] * size
         self.build_tree_util(0, self.length - 1, 0)
     
     def build_tree_util(self, start, end, index):
         if start == end:
+            # Reach the leaf.
             self.tree[index] = self.lane[start]
             return self.lane[start]
         else:
             mid = start + (end - start) / 2
+            # min op is the key. If you want a max segtree then do max.
             self.tree[index] = min(self.build_tree_util(start, mid, index * 2 + 1),
                                    self.build_tree_util(mid + 1, end, index * 2 + 2))
             return self.tree[index]
         
     def query(self, start, end):
+        """Given start/end index, query the min value in this range."""
         if (start < 0 or end >= self.length or start > end):
             raise ValueError
-        return query_util(0, self.length - 1, start, end, 0)
+        return self.query_util(0, self.length - 1, start, end, 0)
     
-    def query_util(start, end , qStart, qEnd, index):
+    def query_util(self, start, end , qStart, qEnd, index):
         if qStart <= start and qEnd >= end:
             return self.tree[index]
         elif end < qStart or start > qEnd:
             return 10000000007 # The range falls out, return a very large number for min op.
         else:
             mid = start + (end - start) / 2
+            # Left/right child relation in tree array is 2 * idx + 1 and 2 * idx + 2.
             return min(self.query_util(start, mid, qStart, qEnd, 2 * index + 1),
                        self.query_util(mid + 1, end, qStart, qEnd, 2 * index + 2))
 
 
-if __name__ == '__main__':
-    lane = [2, 3, 1, 2, 3, 2, 3, 3]
+def main():
+    (N, T) = map(int, raw_input().split())
+    lane = map(int, raw_input().split())
     segtree = SegTree(lane)
-    print segtree.tree
+    for _ in xrange(T):
+        (start, end) = map(int, raw_input().split())
+        print segtree.query(start, end)
+
+
+if __name__ == '__main__':
+    main()
