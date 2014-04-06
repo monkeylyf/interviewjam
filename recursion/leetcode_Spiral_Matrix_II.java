323e166f31f09b5a9e3149bc9693593946487273
Yifeng Liu <laituan1986@gmail.com>
2014-04-06 12:16:53 -0700

    added python version to leetcode_Spiral_Matrix_II.java

--- recursion/leetcode_Spiral_Matrix_II.java
+++ recursion/leetcode_Spiral_Matrix_II.java
@@ -51,3 +51,43 @@ public class leetcode_Spiral_Matrix_II {
         }
     }
 }
+
+/* Python Version
+Iterative way.
+
+def generateMatrix(self, n):
+    matrix = [ [ 0 for _ in xrange(n) ] for _ in xrange(n) ]
+    
+    x = y = 0
+    num = 1
+    
+    while n >= 1:
+        if n == 1:
+            matrix[x][y] = num
+            break
+        # up
+        for i in xrange(y, y + n):
+            matrix[x][i] = num
+            num += 1
+
+        # right
+        for i in xrange(x + 1, x + n - 1):
+            matrix[i][y + n - 1] = num
+            num += 1
+            
+        # down
+        for i in reversed(xrange(y, y + n)):
+            matrix[y + n - 1][i] = num
+            num += 1
+            
+        # left
+        for i in reversed(xrange(x + 1, x + n - 1)):
+            matrix[i][y] = num
+            num += 1
+        
+        n -= 2
+        x += 1
+        y += 1
+    
+    return matrix
+*/
