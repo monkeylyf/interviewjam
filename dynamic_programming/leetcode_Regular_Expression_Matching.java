6acdda32865aa596039003c8785ac04744231385
Yifeng Liu <laituan1986@gmail.com>
2014-04-06 19:29:03 -0700

    added python version to leetcode_Regular_Expression_Matching.java

--- dynamic_programming/leetcode_Regular_Expression_Matching.java
+++ dynamic_programming/leetcode_Regular_Expression_Matching.java
@@ -82,3 +82,54 @@ class leetcode_Regular_Expression_Matching {
         return prev[prev.length - 1];
     }
 }
+
+
+/* Python Version
+class Solution:
+    # @return a boolean
+    def isMatch(self, s, p):
+        pat_tokens = self.parse_pat(p)
+        n = len(pat_tokens)
+        m = len(s)
+
+        dp = [ False ] * (m + 1)
+        dp[0] = True
+
+        for i in xrange(1, n + 1):
+            token = pat_tokens[i - 1]
+            nex = [ False ] * (m + 1)
+            nex[0] = len(token) == 2 and dp[0]
+            for j in xrange(1, m + 1):
+                char = s[j - 1]
+                if len(token) == 2:
+                    if token[0] == '.':
+                        nex[j] = dp[j - 1] or dp[j] or nex[j - 1]
+                    elif token[0] == char:
+                        nex[j] = dp[j - 1] or dp[j] or (j >= 2 and s[j - 2] == char and nex[j - 1])
+                    else:
+                        nex[j] = dp[j]
+                else:
+                    # len(token) == 1
+                    if token == '.' or token == char:
+                        nex[j] = dp[j - 1]
+                    else:
+                        nex[j] = False
+            dp = nex
+
+        return dp[-1]
+
+    def parse_pat(self, pat):
+        pat_tokens = []
+        i = 0
+
+        while i < len(pat):
+            if i + 1 < len(pat) and pat[i + 1] == '*':
+                pat_tokens.append(pat[i : i + 2])
+                i += 1
+            else:
+                pat_tokens.append(pat[i])
+            i += 1
+
+        return pat_tokens
+
+*/
