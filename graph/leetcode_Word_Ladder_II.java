036199a66826f381ef115196daf6a8e0d8ea8a8f
Yifeng Liu <laituan1986@gmail.com>
2014-04-13 21:52:08 -0700

    Rewrote leetcode_Word_Ladder_II.java

--- graph/leetcode_Word_Ladder_II.java
+++ graph/leetcode_Word_Ladder_II.java
@@ -12,10 +12,10 @@ end = "cog"
 dict = ["hot","dot","dog","lot","log"]
 Return
   [
-      ["hit","hot","dot","dog","cog"],
-      ["hit","hot","lot","log","cog"]
+  ["hit","hot","dot","dog","cog"],
+  ["hit","hot","lot","log","cog"]
   ]
-*/
+ */
 
 
 import java.util.ArrayList;
@@ -29,93 +29,190 @@ import java.util.Iterator;
 
 public class leetcode_Word_Ladder_II {
 
-    public static void main(String[] args) {
-        HashSet<String> dict1 = new HashSet<String>();
-        Collections.addAll(dict1, "ted","tex","red","tax","tad","den","rex","pee");
-        test("red", "tax", dict1);
-        HashSet<String> dict2 = new HashSet<String>();
-        Collections.addAll(dict2, "hot", "dot");
-        test("hot", "hot", dict2);
-    }
+  public static void main(String[] args) {
+	leetcode_Word_Ladder_II instance = new leetcode_Word_Ladder_II();
+	instance.solve();
+  }
 
-    public static ArrayList<ArrayList<String>> test(String start, String end, HashSet<String> dict) {
-        // The idea behind this is BFS + caching paths.
-        // Since the shortest path is asked for then DFS is not an efficienty way to solve this.
-        // Need two HashMap path & tmPath, one for global path and another for path at current level.
-        // Need two Queue curLevel & nextTovVisit to record graph level.
-        HashMap<String, ArrayList<String>> path = new HashMap<String, ArrayList<String>>();
-        HashMap<String, ArrayList<String>> tmpPath = new HashMap<String, ArrayList<String>>();
-        Queue<String> curLevel = new LinkedList<String>();
-        Queue<String> nextLevel = new LinkedList<String>();
-        curLevel.add(start);
-        boolean isShortestLevel = false;
-        while (!curLevel.isEmpty()) {
-            String str = curLevel.remove();
-            for (int i = 0; i < start.length(); ++i) {
-                for(char j = 'a'; j <= 'z'; ++j){
-                    // Change the str then check if the dict contains it.
-                    char[] chs = str.toCharArray();
-                    chs[i] = j;
-                    String oneCharDiff = new String(chs);
-                    if (oneCharDiff.equals(str)) {
-                        continue; // new string is exactly same as the unchanged.
-                    }
-                    if (oneCharDiff.equals(end)) {
-                        isShortestLevel = true; // Reach the shortest path.
-                    }
-                    if (!path.containsKey(oneCharDiff) && dict.contains(oneCharDiff)) {
-                        if (!tmpPath.containsKey(oneCharDiff)) {
-                            ArrayList<String> arr = new ArrayList<String>();
-                            arr.add(str);
-                            tmpPath.put(oneCharDiff, arr);
-                            nextLevel.add(oneCharDiff);
-                        } else {
-                            ArrayList<String> arr = tmpPath.get(oneCharDiff);
-                            arr.add(str);
-                            tmpPath.put(oneCharDiff, arr);
-                        }
-                    }
-                }
-            }
-            if (curLevel.isEmpty()) {
-                Queue<String> tmp = nextLevel; // Swap nextLevel and curLevel.
-                nextLevel = curLevel;
-                curLevel = tmp;
-                path.putAll(tmpPath); // Move all paths hashed as this level to global path map.
-                tmpPath = new HashMap<String, ArrayList<String>>(); // tmpPath.clear()
-                if (isShortestLevel) {
-                    return dfsPath(path, start, end); // Finished with level with shortest path. Start reconstructing path.
-                }
-            }
-        }
-        return new ArrayList<ArrayList<String>>();
-    }
+  public void solve() {
+	HashSet<String> dict1 = new HashSet<String>();
+	Collections.addAll(dict1, "ted","tex","red","tax","tad","den","rex","pee");
+	System.out.println(findLadders("red", "tax", dict1));
+	HashSet<String> dict2 = new HashSet<String>();
+	Collections.addAll(dict2, "hot", "dot");
+	System.out.println(findLadders("hot", "hot", dict2));
+  }
+  public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
+	Queue<String> q = new LinkedList<String>();
+	HashMap<String, ArrayList<String>> global = new HashMap<String, ArrayList<String>>();
+	HashMap<String, ArrayList<String>> local = new HashMap<String, ArrayList<String>>();
 
-    public static ArrayList<ArrayList<String>> dfsPath(HashMap<String, ArrayList<String>> path, String start, String end) {
-        // Standord DFS.
-        ArrayList<ArrayList<String>> all = new ArrayList<ArrayList<String>>();
-        ArrayList<String> one = new ArrayList<String>();
-        one.add(start);
-        nextPath(path, start, end, all, one, 0);
-        for (ArrayList<String> i : all) System.out.println(i);
-        return all;
-    }
+	q.add(start);
+	dict.add(end);
 
-    public static void nextPath(HashMap<String, ArrayList<String>> path, String start, String end, ArrayList<ArrayList<String>> all, ArrayList<String> one, int level) {
-        // :param level: type int. for corner cases like "hot", "hot", ["hot", "dot"]
-        // At the first level start might equal to end, but the recursion should keep going.
-        if (end.equals(start) && level > 0) {
-            ArrayList<String> tmp = new ArrayList<String>();
-            for (String i : one) {
-                tmp.add(i);
-            }
-            all.add(tmp);
-        } else {
-            one.add(1, end);
-            for (String i : path.get(end)) {
-                nextPath(path, start, i, all, one, level + 1);
-            }
-            one.remove(1);
-        }
-    }
+	boolean finished = false;
+
+	char[] charPool = new char[26];
+	for (int i = 'a'; i <= 'z'; ++i) {
+	  charPool[i - 'a'] = (char) i;
+	}
+
+	int count = 1;
+
+	while (!q.isEmpty()) {
+	  String cur = q.poll();
+	  for (String word : wordGen(cur, charPool, dict)) {
+		if (word.equals(end) && !finished) {
+		  finished = true;
+		}
+
+		// Create footprint.
+		if (!global.containsKey(word)) {
+		  ArrayList<String> localPath = local.get(word);
+		  if (localPath == null) {
+			localPath = new ArrayList<String>();
+			q.add(word);
+		  }
+		  localPath.add(cur);
+		  local.put(word, localPath);
+		}
+	  }
+
+	  count -= 1;
+
+	  if (count == 0) {
+		count = q.size();
+		global.putAll(local);
+
+		if (finished) {
+		  return collect(global, start, end);
+		}
+
+		local = new HashMap<String, ArrayList<String>>();
+	  }
+	}
+
+	return new ArrayList<ArrayList<String>>();
+  }
+
+  private ArrayList<ArrayList<String>> collect(HashMap<String, ArrayList<String>> path, String start, String end) {
+	ArrayList<ArrayList<String>> container = new ArrayList<ArrayList<String>>();
+	ArrayList<String> acc = new ArrayList<String>();
+	acc.add(end);
+	dfs(start, end, container, acc, path);
+	return container;
+  }
+
+  private void dfs(String start, String word, ArrayList<ArrayList<String>> container, ArrayList<String> acc, HashMap<String, ArrayList<String>> path) {
+	if (word.equals(start)) {
+	  ArrayList<String> sol = new ArrayList<String>();
+	  for (int i = acc.size() - 1; i >= 0; --i) {
+		sol.add(acc.get(i));
+	  }
+	  container.add(sol);
+	} else {
+	  for (String prevWord : path.get(word)) {
+		acc.add(prevWord);
+		dfs(start, prevWord, container, acc, path);
+		acc.remove(acc.size() - 1);
+	  }
+	}
+  }
+
+  private ArrayList<String> wordGen(String word, char[] charPool, HashSet<String> dict) {
+	ArrayList<String> container = new ArrayList<String>();
+	char[] arr = word.toCharArray();
+	for (int i = 0; i < arr.length; ++i) {
+	  for (int j = 0; j < charPool.length; ++j) {
+		if (arr[i] == charPool[j]) {
+		  continue;
+		}
+		arr[i] = charPool[j];
+		String newStr = new String(arr);
+		if (dict.contains(newStr)) {
+		  container.add(newStr);
+		}
+	  }
+	  arr = word.toCharArray();
+	}
+
+	return container;
+  }
 }
+
+/* Python Version (Compile Error. WTF?)
+
+class Solution:
+    # @param start, a string
+    # @param end, a string
+    # @param dict, a set of string
+    # @return a list of lists of string
+    def findLadders(self, start, end, dict):
+        global_path = {}
+        level_path = {}
+
+        dict.add(end)
+
+        pipe = [start]
+
+        char_pool = [ chr(i) for i in xrange(97, 123) ]
+
+        finished = False
+        count = 1
+
+        while pipe:
+            w = pipe.pop(0)
+            for next_word in self.nextTran(w, dict, char_pool):
+                if next_word == end:
+                    finished = True
+                    try:
+                        level_path[next_word].append(w)
+                    except KeyError:
+                        level_path[next_word] = [w]
+
+                elif not next_word in global_path:
+                    try:
+                        level_path[next_word].append(w)
+                    except KeyError:
+                        level_path[next_word] = [w]
+                    pipe.append(next_word)
+
+            count -= 1
+            if count == 0:
+                count = len(pipe)
+                for k, v in level_path.iteritems():
+                    global_path[k] = v
+                level_path = {}
+                if finished:
+                    break
+
+        #print global_path
+        if not global_path:
+            return []
+        else:
+            return self.collect(global_path, end, start)
+
+    def nextTran(self, word, dict, char_pool):
+        """Generater."""
+        for i in xrange(len(word)):
+            for char in char_pool:
+                if char == word[i]:
+                    continue
+                ns = word[:i] + char + word[i + 1:]
+                if ns in dict:
+                    yield ns
+
+    def collect(self, global_path, end, start):
+        def dfs(global_path, word, acc, container, start):
+            if word == start:
+                container.add(':'.join(acc[::-1]))
+            else:
+                for prev in global_path[word]:
+                    acc.append(prev)
+                    dfs(global_path, prev, acc, container, start)
+                    acc.pop()
+
+        container = set()
+        dfs(global_path, end, [end], container, start)
+        return [ token.split(':') for token in container ]
+*/
