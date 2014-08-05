dbfb3ed0ce14335d209919baa29fdeb632f9110f
Yifeng Liu <laituan1986@gmail.com>
2014-08-04 19:30:46 -0700

    added stats.sh the screencast by Gary

---
+++ stats.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+file_pattern=$1
+
+main() {
+    for rev in `revisions`; do
+        echo "`number_of_lines` `commit_description`"
+    done
+}
+
+revisions() {
+    git rev-list --reverse HEAD
+}
+
+commit_description() {
+    git log --oneline -1 ${rev}
+}
+
+number_of_lines() {
+    git ls-tree -r ${rev} |
+    grep "${file_pattern}" |
+    awk '{print $3}' |
+    xargs git show |
+    wc -l
+}
+
+main
