6e0fe76dd1cb708fce477e663746db3140349331
monkeylyf <laituan1986@gmail.com>
2014-10-07 17:45:56 -0700

    added hackerrank/hackerrank_Strange_Numbers.py

---
+++ hackerrank/hackerrank_Strange_Numbers.py
@@ -0,0 +1,66 @@
+"""hackerrank_Strange_Numbers.py
+
+https://www.hackerrank.com/contests/w11/challenges/strange-numbers
+"""
+
+
+def strange_num_generator(n, batch=None):
+    """The idea is construct all strange numbers instead of iterating through
+    every the numbers from 1 to 10 ** 18 to check.
+
+    Base on the definition of strange numbers, numbers with length n - 1, multiplied
+    by n, if the results are of length n, then they are strange numbers as well.
+    For those new strange numbers with length n, multiplied by n, if the results are
+    still of length n, then they are strange numbers as well. Looping until you see
+    results with length n + 1.
+
+    :param n:int, the number of digits of a number, which represents a range.
+    :param batch;list, all the strange numbers with n - 1 length.
+
+    :return pool:list, all the strange numbers with n length.
+    """
+    if n == 1:
+        return range(10)
+
+    if not batch:
+        return []
+
+    pool = []
+    threshold = 10 ** (n - 1) / n
+    batch = filter(lambda x : x >= threshold, batch)
+
+    uplimit = 10 ** n
+
+    while 1:
+        batch = map(lambda x : x * n, batch)
+        if batch[-1] > uplimit:
+            pool += filter(lambda x : x < uplimit, batch)
+            break
+        else:
+            pool += batch
+
+    return pool
+
+
+def main():
+    """Pre-compute all strange numbers and hard-code them in the script will work
+    as well but it's not elegent.
+    """
+    strange_number_pool = []
+    pool = None
+    for i in xrange(1, 18 + 1):
+        # length of the numnber is in [1, 18]
+        pool = strange_num_generator(i, batch=pool)
+        strange_number_pool += pool
+
+    for _ in xrange(int(raw_input())):
+        l, r = map(int, raw_input().split())
+        count = 0
+        for i in strange_number_pool:
+            if l <= i and i <= r:
+                count += 1
+        print count
+
+
+if __name__ == '__main__':
+    main()
