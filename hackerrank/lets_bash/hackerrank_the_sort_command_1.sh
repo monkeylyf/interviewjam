245954cf959a07712953217dcc7d25958e2bfce6
monkeylyf <laituan1986@gmail.com>
2014-10-28 16:39:12 -0700

    added sort command challenge

---
+++ hackerrank/lets_bash/hackerrank_the_sort_command_1.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# hackerrank_the_sort_command_1
+#
+# https://www.hackerrank.com/contests/lets-bash/challenges/text-processing-sort-1
+
+set -e
+
+main() {
+    sort
+}
+
+main
---
+++ hackerrank/lets_bash/hackerrank_the_sort_command_2.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# hackerrank_the_sort_command_2
+#
+# https://www.hackerrank.com/contests/lets-bash/challenges/text-processing-sort-2
+
+set -e
+
+main() {
+    sort -r
+}
+
+main
