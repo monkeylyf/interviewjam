e3b105abeb41fa74ef421993e957d6199c96d02d
monkeylyf <laituan1986@gmail.com>
2014-12-06 17:20:44 -0800

    added leetcode_Find_Peak_Element.py
    Mark Find_Peak.java as duplciate.

---
+++ searching/leetcode_Find_Peak_Element.py
@@ -0,0 +1,46 @@
+"""leetcode_Find_Peak_Element
+leetcode
+
+peak element is an element that is greater than its neighbors.
+Given an input array where num[i] is not equal to num[i+1], find a peak element
+and return its index.
+The array may contain multiple peaks, in that case return the index to any one
+of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = negative infinity.
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should
+return the index number 2.
+"""
+
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def findPeakElement(self, num):
+        """Binary search. Time complexity O(nlgn)."""
+        head = 0
+        tail = len(num) - 1
+
+        while head <= tail:
+            if head == tail:
+                return head # Edge case: [0], head = 0, tail = 0
+            if head + 1 == tail:
+                # Return the index of larger element.
+                return head if num[head] > num[tail] else tail
+
+            mid = (tail - head) / 2 + head
+
+            # Now mid can not be 0 or n - 1.
+            # For mid == 0, then head = 0, tail = 0 or 1
+            # For mid == n - 1, then head = n - 1 and tail = n - 1.
+            if num[mid - 1] > num[mid]:
+                tail = mid - 1
+            elif num[mid] < num[mid + 1]:
+                head = mid + 1
+            else:
+                # Hit here then: num[mid - 1] > num[mid] < mid[mid + 1]
+                # Find peak.
+                return mid
+
+        # I don't think the function will ever hit here but make sure
+        # it does not return None.
+        return head
