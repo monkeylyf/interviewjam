b5087534528dbf76e395a43c90a4abea34a1cf17
monkeylyf <laituan1986@gmail.com>
2015-01-01 19:35:32 -0800

    revised dynamic_programming/leetcode_Unique_Binary_Search_Trees.java

--- dynamic_programming/leetcode_Unique_Binary_Search_Trees.java
+++ dynamic_programming/leetcode_Unique_Binary_Search_Trees.java
@@ -1,67 +1,81 @@
-/*Unique_Binary_Search_Trees
-
-Given n, how many structurally unique BST's (binary search trees) that store
-values 1...n?
-For example,
-Given n = 3, there are a total of 5 unique BST's.
-
-   1         3     3      2      1
-    \       /     /      / \      \
-     3     2     1      1   3      2
-    /     /       \                 \
-   2     1         2                 3
-
-*/
+/**
+ * Unique_Binary_Search_Trees.
+ * leetcode
+ *
+ * Given n, how many structurally unique BST's (binary search trees) that store
+ * values 1...n?
+ * For example,
+ * Given n = 3, there are a total of 5 unique BST's.
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ * Catalan number: http://en.wikipedia.org/wiki/Catalan_number
+ */
 
 
 
 public class leetcode_Unique_Binary_Search_Trees {
 
-    public static void main(String[] args) {
-        System.out.println(numTrees(10));
+  public static void main(String[] args) {
+    leetcode_Unique_Binary_Search_Trees solution = new leetcode_Unique_Binary_Search_Trees();
+    solution.test();
+  }
+
+  public void test() {
+    System.out.println(numTrees(10));
+  }
+
+  /**
+   * The idea behind this is dp-based.
+   * numTrees(i+1) = append node i+1 to node i's right + append tree(0,i) as node i+1 left child.
+   *                 insert the last node as non-leaf node sum(k=1...i)(dp(k-1)*dp(i-k)
+   * if(n == 0 || n == 1 || n == 2) {
+   */
+  public int numTrees(int n) {
+      return n;
+    }
+    int dp[] = new int[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+    //   1  2
+    //  /    \
+    // 2      1
+    for (int i = 3; i <= n; ++i) {
+      // Insert the value in the middle.
+      for (int j = 1; j <= i - 2; ++j) {
+        // Consider node i as the intermediate node as a right child of some subtree.
+        // Then create another substree and append its root to node i.
+        // There is dp[j] different ways to construct first substree and dp[i - 1 - j]
+        // different ways to construct the second substree. Sum the num up.
+        dp[i] += dp[j] * dp[i - 1 - j];
+      }
+
+      // Insert the value on the top or bottum.
+      dp[i] += 2 * dp[i - 1];
     }
 
-    public static int numTrees(int n) {
-        // The idea behind this is dp-based.
-        // numTrees(i+1) = append node i+1 to node i's right + append tree(0,i) as node i+1 left child.
-        //                 insert the last node as non-leaf node sum(k=1...i)(dp(k-1)*dp(i-k)
-        if(n == 0 || n == 1 || n == 2) {
-            return n;
-        }
-        int dp[] = new int[n + 1];
-		dp[1] = 1;
-		dp[2] = 2;
-        //   1  2
-        //  /    \
-        // 2      1
-        for (int i = 3; i <= n; ++i) {
-			for (int j = 1; j <= i - 2; ++j) {
-				// Consider node i as the intermediate node as a right child of some subtree.
-				// Then create another substree and append its root to node i.
-				// There is dp[j] different ways to construct first substree and dp[i - 1 - j] 
-				// different ways to construct the second substree. Sum the num up.
-				dp[i] += dp[j] * dp[i - 1 - j];
-			}
-			dp[i] += 2 * dp[i - 1];
-        }
-
-        return dp[n];
-    }
+    return dp[n];
+  }
 }
 
 
-/* Python Version
-def numTrees(self, n):
-    if n == 0 or n == 1 or n == 2:
-        return n
-    
-    dp = [ 0 for _ in xrange(n + 1) ]
-    dp[1] = 1
-    dp[2] = 2
-    for i in xrange(3, n + 1):
-        for j in xrange(1, i - 1):
-            dp[i] += dp[j] * dp[i - j - 1]
-        dp[i] += 2 * dp[i - 1]
-
-    return dp[-1]
-*/
+/**
+ * Python Version.
+ * def numTrees(self, n):
+ *     if n == 0 or n == 1 or n == 2:
+ *         return n
+ *
+ *     dp = [ 0 for _ in xrange(n + 1) ]
+ *     dp[1] = 1
+ *     dp[2] = 2
+ *     for i in xrange(3, n + 1):
+ *         for j in xrange(1, i - 1):
+ *             dp[i] += dp[j] * dp[i - j - 1]
+ *         dp[i] += 2 * dp[i - 1]
+ *
+ *     return dp[-1]
+ */
