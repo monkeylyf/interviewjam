ddc59ff013153a319d53b7c5d107c3884408cdb3
monkeylyf <laituan1986@gmail.com>
2015-01-10 11:49:20 -0800

    revised arr/cap_Pairs.java and arr/cap_Swap_Numbers.java

--- arr/cap_Swap_Numbers.java
+++ arr/cap_Swap_Numbers.java
@@ -1,25 +1,29 @@
-/*Swap_Numbers
-careercup
+/**
+ * Swap_Numbers.
+ * careercup
+ *
+ * Write a function to swap a number in place without temporary variables
+ */
 
-Write a function to swap a number in place without temporary variables
-*/
+public class cap_Swap_Numbers {
 
-class cap_Swap_Numbers {
-	public static void main(String[] args) {
-		swap(3, 1);
-		swapInt(3, 1);
-	}
-	public static int[] swap(int a, int b) {
-        // Swap numbers in array is the most common operations.
-		a = a - b;
-		b = b + a;
-		a = b - a;
-		return new int[] {a, b};
-	}
-	public static int[] swapInt(int a, int b) {
-		a = a ^ b;
-		b = a ^ b;
-		a = a ^ b;
-		return new int[] {a, b};
-	}
+  public static void main(String[] args) {
+    swap(3, 1);
+    swapInt(3, 1);
+  }
+
+  public static int[] swap(int a, int b) {
+    // Swap numbers in array is the most common operations.
+    a = a - b;
+    b = b + a;
+    a = b - a;
+    return new int[] {a, b};
+  }
+
+  public static int[] swapInt(int a, int b) {
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    return new int[] {a, b};
+  }
 }
