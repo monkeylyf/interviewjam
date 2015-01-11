7b739bf7408fa733cc785d45aaf3821ec199fcbf
monkeylyf <laituan1986@gmail.com>
2015-01-10 18:09:58 -0800

    revised dynamic_programming/leetcode_Word_Break.java and dynamic_programming/leetcode_Word_Break_II.py

--- dynamic_programming/leetcode_Word_Break_II.py
+++ dynamic_programming/leetcode_Word_Break_II.py
@@ -1,11 +1,14 @@
-# leetcode_Word_Break_II
+"""leetcode_Word_Break_II.py
+leetcode
+
+"""
 
 
 class Solution:
     # @param s, a string
-    # @param dict, a set of string
+    # @param dikt, a set of string
     # @return a list of strings
-    def wordBreak(self, s, dict):
+    def wordBreak(self, s, dikt):
         # Nested function
         def dfs(idx, acc):
             """Given the trace, dfs to collect all possible results."""
@@ -17,18 +20,21 @@ class Solution:
                 else:
                     dfs(tail, new_acc)
 
-        # trace records the start/stop index of all words in dict.
-        trace = [ [] for _ in xrange(len(s)) ]
-        
-        # 1. Scan all substring s[i:] first to see if they are in dict.
-        # 2. Continue scan backwards. if s[i:] in dict, check all s[j:i]
-        # Basically it's backwards dp to track the start/end index of substr in dict.
+        # trace records the start/stop index of all words in dikt.
+        # When it ready, this question turns to be finding all path in graph.
+        trace = [None] * len(s)
+
+        # 1. Scan all substring s[i:] first to see if they are in dikt.
+        # 2. Continue scan backwards. if s[i:] in dikt, check all s[j:i]
+        # Basically it's backwards dp to track the start/end index of substr in dikt.
         for tail in reversed(xrange(len(s) + 1)):
-            if tail < len(s) and not trace[tail]:
-                continue
-            for head in reversed(xrange(tail)):
-                if s[head : tail] in dict:
-                    trace[head].append(tail)
+            if tail >= len(s) or trace[tail]:
+                for head in reversed(xrange(tail)):
+                    if s[head : tail] in dikt:
+                        try:
+                            trace[head].append(tail)
+                        except AttributeError:
+                            trace[head] = [tail]
 
         container = []
         dfs(0, '')
@@ -50,27 +56,27 @@ if __name__ == '__main__':
 
 
 class Solution {
-  public ArrayList<String> wordBreak(String s, Set<String> dict) {
-    ArrayList<ArrayList<Integer>> trace = new ArrayList<ArrayList<Integer>>();
+  public ArrayList<String> wordBreak(String s, Set<String> dikt) {
+    List<List<Integer>> trace = new ArrayList<ArrayList<Integer>>();
     for (int i = 0; i < s.length(); ++i) {
       trace.add(new ArrayList<Integer>());
     }
-    
+
     for (int tail = s.length(); tail >= 0; --tail) {
       if (tail < s.length() && trace.get(tail).isEmpty()) { continue; }
       for (int head = tail - 1; head >= 0; --head) {
-        if (dict.contains(s.substring(head, tail))) {
+        if (dikt.contains(s.substring(head, tail))) {
           trace.get(head).add(tail);
         }
       }
     }
 
-    ArrayList<String> container = new ArrayList<String>();
+    List<String> container = new ArrayList<String>();
     dfs(trace, "", 0, container, s);
     return container;
   }
-    
-  public void dfs(ArrayList<ArrayList<Integer>> trace, String acc, int head, ArrayList<String> container, String s) {
+
+  private void dfs(List<List<Integer>> trace, String acc, int head, List<String> container, String s) {
     if (head == s.length()) {
       container.add(acc);
     } else {
