57ab76ab9e66bf6fb4c171f8849a0b26b99f68e4
monkeylyf <laituan1986@gmail.com>
2015-01-11 18:46:13 -0800

    revised arr/cap_Maximum_Submatrix.java
    Fixed a bug and refactored a little bit.

--- arr/cap_Maximum_Submatrix.java
+++ arr/cap_Maximum_Submatrix.java
@@ -1,52 +1,94 @@
-/*Maximum_Submatrix
-careercup
+/**
+ * Maximum_Submatrix.
+ * careercup.
+ *
+ * Given an NxN matrix of positive and negative integers, write code to find the
+ * sub-matrix with the largest possible sum
+ */
 
-Given an NxN matrix of positive and negative integers, write code to find the
-sub-matrix with the largest possible sum
-*/
+public class cap_Maximum_Submatrix {
 
-class cap_Maximum_Submatrix {
-	public static void main(String[] args) {
-		int[][] input = {{1, 2, -3},
-			  {3, -2, 1},
-			  {1, -1, 1}};
-		System.out.println(getMaxMatrix(input));
-		int[][] input1 = {{1, 1, 1},
-				  {-1, -1, -1},
-				  {-2, -2, -2}};
-		System.out.println(getMaxMatrix(input1));
-		int[][] input2 = {{1, 2, -3},
-			  	  {3, -2, 1},
-			  	  {1, -1, 1},
-			  	  {3, 0, 4}};
-		System.out.println(getMaxMatrix(input2));
-	}
-	public static int getMaxMatrix(int[][] mtx) {
-        // Brutal force: O(n^6): O(n^4) to traversal all possible submatrix and O(n^2) to compute the sum
-        // O(n^3) as follow
-        int n = mtx.length;
-        int m = mtx[0].length;
-		int max = 0;
-        int[] accumulator;
-		for (int i = 0; i < n; ++i) {
-			accumulator = new int[n];
-			for (int j = i; j < n; ++j) {
-                // Given the submatrix with upper row [i] and lower row[n-1] and random left/right borders.
-                // Calculate the maximum sum.
-				for (int it = 0; it < m; ++it) {
-                    accumulator[it] += mtx[j][it]; // accumulator the value along y axis.
-                }
-				int sum = 0; // Paradigm: Maximum subarray.
-				for (int len = 0; len < m; ++len) {
-					sum += accumulator[len];
-					if (sum > max) {
-                        max = sum;
-                    } else if (sum < 0) {
-                        sum = 0;
-                    }
-				}
-			}
-		}
-		return max;
-	}
+  public static void main(String[] args) {
+    cap_Maximum_Submatrix solution = new cap_Maximum_Submatrix();
+    solution.test();
+  }
+
+  public void test() {
+    // test case 1.
+    int[][] input = {
+      {1, 2, -3},
+      {3, -2, 1},
+      {1, -1, 1}};
+    //System.out.println(getMaxMatrix(input));
+
+    // test case 2.
+    int[][] input1 = {
+      {1,  1,  1},
+      {-1, -1, -1},
+      {-2, -2, -2}};
+    //System.out.println(getMaxMatrix(input1));
+
+    // test case 3.
+    int[][] input2 = {{1, 2, -3},
+      {3, -2, 1},
+      {1, -1, 1},
+      {3, 0, 4}};
+    //System.out.println(getMaxMatrix(input2));
+
+    // test case 4.
+    int[][] input3 = {
+      {1, 2, -1, -4, -20},
+      {-8, -3, 4, 2, 1},
+      {3, 8, 10, 1, 3},
+      {-4, -1, 1, 7, -6},
+    };
+    System.out.println(getMaxMatrix(input3));
+  }
+
+  /** Dynamic programming.
+   *
+   * Brutal force: O(n^6).
+   * O(n^4) to traversal all possible submatrix and O(n^2) to compute the sum
+   *
+   * O(n^3) as follow
+   */
+  public int getMaxMatrix(int[][] mtx) {
+    int n = mtx.length;
+    int m = mtx[0].length;
+    int max = 0;
+    for (int i = 0; i < n; ++i) {
+      int[] accumulator = new int[m];
+      for (int j = i; j < n; ++j) {
+        // Given the submatrix with upper bound i and lower bound j,
+        // calculate the maximum sum.
+        for (int it = 0; it < m; ++it) {
+          accumulator[it] += mtx[j][it]; // accumulator the value along y axis.
+        }
+
+        max = Math.max(max, maxSumOfArray(accumulator));
+      }
+    }
+
+    return max;
+  }
+
+  /**
+   * Paradigm: Maximum subarray.
+   */
+  private int maxSumOfArray(int[] arr) {
+    int max = 0;
+    int sum = 0;
+    for (int i = 0; i < arr.length; ++i) {
+      sum += arr[i];
+      if (sum > max) {
+        max = sum;
+      } else if (sum < 0) {
+        sum = 0;
+      } else {
+        continue;
+      }
+    }
+
+    return max;
+  }
 }
