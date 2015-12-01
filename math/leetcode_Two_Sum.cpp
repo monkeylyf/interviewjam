ebe204a3b2696641ffa6c3cff1bc7c465a092d9a
monkeylyf <laituan1986@gmail.com>
2015-12-01 12:23:46 -0800

    math:two sum redo in cpp

---
+++ math/leetcode_Two_Sum.cpp
@@ -0,0 +1,58 @@
+/*
+ * leet code Two Sum
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <map>
+
+using std::vector;
+using std::map;
+
+
+void print_vector(const vector<int> *v) {
+    for (int val : *v) {
+        printf("%d ", val);
+    }
+    printf("\n");
+}
+
+
+class Solution {
+    public:
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mapping;
+
+        int idx = 0;
+
+        for (int val : nums) {
+            auto iter = mapping.find(target - val);
+
+            if (iter != mapping.end()) {
+                // Find match.
+                int known_idx = iter->second;
+
+                vector<int> res {known_idx, idx};
+                return res;
+            }
+
+            mapping[val] = idx;
+            ++idx;
+        }
+
+        vector<int> res;
+        return res;
+    }
+};
+
+
+int main() {
+    Solution sol = Solution();
+
+    vector<int> nums {2, 3, 1, 6, 8};
+    int target = 8;
+    vector<int> res = sol.twoSum(nums, target);
+    print_vector(&res);
+    return 0;
+}
