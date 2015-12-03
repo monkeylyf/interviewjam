f30618b79c8da26dbc1e5ee4679562dd63a89ca7
monkeylyf <laituan1986@gmail.com>
2015-12-02 20:19:05 -0800

    str:longest substring w/o repeating charactors
    So, the algo I used for Python/Java won't pass for cpp. The reason are
    two:
    1. std::map is O(lgn) for find. Python/Java dict/HashMap is O(1). So for
       cpp a mask should be used to achieve O(1) time complexity given that
       it's asciII.
    2. Algo for Java/Python the dict/HashMap is complete reset when seeing a
       duplicate char and move the pointer back to seen index + 1, which is
       a little bit waste because the range between current index and seen
       index + 1 will be re-calculate, which is unnecessary.
    
    The most efficient way for cpp/java/python is using two pointers. When
    seeing a dup char, move the left pointer until you see the char that
    right pointer is currently pointing to.

---
+++ str/leetcode_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015
+ * Author: ouroboros
+ *
+ * leetcode longest substring w/o repeatin'chars
+ */
+
+#include <algorithm>
+#include <string>
+
+using std::string;
+using std::max;
+
+
+class Solution {
+ public:
+    int lengthOfLongestSubstring(string s) {
+      bool seen[256] = { false };
+      int start = 0;
+      int end = 0;
+      int ret = 0;
+      while (end < s.length()) {
+        if (seen[s[end]]) {
+          ret = max(ret, end - start);
+          while (s[start] != s[end]) {
+            seen[s[start]] = false;
+            ++start;
+          }
+          ++start;
+        } else {
+          seen[s[end]] = true;
+        }
+
+        ++end;
+      }
+      return max(ret, end - start);
+    }
+};
+
+
+
+int main() {
+  Solution sol;
+  int len = sol.lengthOfLongestSubstring("abcabcbb");
+  if (len != 3) {
+    throw 1;
+  }
+  return 0;
+}
