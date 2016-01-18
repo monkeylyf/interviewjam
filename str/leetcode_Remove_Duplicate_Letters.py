277920a35cd7aef0fee81cfb586e61a813fa99c3
monkeylyf <laituan1986@gmail.com>
2016-01-17 20:14:05 -0800

    str: remove duplicate letters

---
+++ str/leetcode_Remove_Duplicate_Letters.py
@@ -0,0 +1,52 @@
+"""Remove duplicate letters
+leetcode
+
+Given a string which contains only lowercase letters, remove duplicate letters
+so that every letter appear once and only once. You must make sure your result
+is the smallest in lexicographical order among all possible results.
+"""
+
+
+
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # char to index mapping. 0-based.
+        mapping = {c: ord(c) - ord('a') for c in 'abcdefghijklmnopqrstuvwxyz'}
+        counter = [0] * 26
+        used = [False] * 26
+        for char in s:
+            idx = mapping[char]
+            counter[idx] += 1
+
+        stack = []
+        for char in s:
+            idx = mapping[char]
+            counter[idx] -= 1
+            if used[idx]:
+                continue
+
+            # Keep stack monotone increasing at best effort.
+            # If current char is smaller the top of stack, and top of stack char
+            # will still be seen later, replace it with current char.
+            while stack and stack[-1] > char and counter[mapping[stack[-1]]] > 0:
+                removed_c = stack.pop()
+                used[mapping[removed_c]] = False
+
+            used[idx] = True
+            stack.append(char)
+
+        return ''.join(stack)
+
+
+def main():
+    sol = Solution()
+    assert sol.removeDuplicateLetters('cbacdcbc') == 'acdb'
+    assert sol.removeDuplicateLetters('bcabc') == 'abc'
+
+
+if __name__ == '__main__':
+    main()
