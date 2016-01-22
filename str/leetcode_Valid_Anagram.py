f7aeb3c1830369118ddaa37a2c904f3df56912bc
monkeylyf <laituan1986@gmail.com>
2016-01-21 18:53:05 -0800

    str: valid anagram
    if implemented in cpp, are int array with length 256 is needed as a
    counter.
    Since I am coding in Python so fuck that and use Counter from
    collections module.

---
+++ str/leetcode_Valid_Anagram.py
@@ -0,0 +1,31 @@
+"""Valid anagram
+leetcode
+
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+"""
+
+from collections import Counter
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return Counter(s) == Counter(t)
+
+
+def main():
+    sol = Solution()
+
+
+if __name__ == '__main__':
+    main()
