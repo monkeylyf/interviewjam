c09443cc36af9db8e454b35562d4497756c7b424
monkeylyf <laituan1986@gmail.com>
2016-01-24 10:46:18 -0800

    graph: course schedule

--- graph/Strongly_Connected_Components.java
+++ graph/Strongly_Connected_Components.java
@@ -1,127 +1,131 @@
-/*Stringly_Conncted_Components
+/**
+ * Stringly_Conncted_Components.
+ *
+ * Wiki for Kosaraju's algorithm::
+ * http://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
+ */
 
-Wiki for Kosaraju's algorithm::
-http://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
-*/
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Strongly_Connected_Components {
 
-	/**
-	 * A directed graph is strongly connected if there is a path between all
-	 * pairs of vertices. A strongly connected component (SCC) of a directed
-	 * graph is a maximal strongly connected subgraph.
-	 */
-	public static void main(String[] args) {
-		SCCGraph g = new SCCGraph(5);
-		// 1 --> 0 ---> 3
-		// ^   /        |
-		// |  / 	    |
-		// | /		    \/
-		// 2<	    	4
-	    g.addEdge(1, 0);
-	    g.addEdge(0, 2);
-	    g.addEdge(2, 1);
-	    g.addEdge(0, 3);
-	    g.addEdge(3, 4);
-	    g.printSCC();
-	}
+  /**
+   * A directed graph is strongly connected if there is a path between all
+   * pairs of vertices.
+   *
+   * A strongly connected component (SCC) of a directed graph is a maximal
+   * strongly connected subgraph.
+   */
+  public static void main(String[] args) {
+    SCCGraph g = new SCCGraph(5);
+    // 1 --> 0 ---> 3
+    // ^   /        |
+    // |  / 	    |
+    // | /		    \/
+    // 2<	    	4
+    g.addEdge(1, 0);
+    g.addEdge(0, 2);
+    g.addEdge(2, 1);
+    g.addEdge(0, 3);
+    g.addEdge(3, 4);
+    g.printSCC();
+  }
 }
 
 class SCCGraph {
-	int V;
-	ArrayList<ArrayList<Integer>> adj; // Use adjacency list to represent a graph.
+  int V;
+  List<List<Integer>> adj; // Use adjacency list to represent a graph.
 
-	SCCGraph(int V) { // Constructor.
-		this.V = V;
-		this.adj = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> tmp;
-		for (int i = 0; i < V; ++i) {
-			tmp = new ArrayList<Integer>();
-			this.adj.add(tmp);
-		}
-	}
+  SCCGraph(int V) { // Constructor.
+    this.V = V;
+    this.adj = new ArrayList<>();
+    for (int i = 0; i < V; ++i) {
+      List<Integer> tmp = new ArrayList<>();
+      this.adj.add(tmp);
+    }
+  }
 
-	public void printSCC() {
-		boolean[] visited = new boolean[this.V];
-		Stack<Integer> s = new Stack<Integer>();
-		//
-		for (int i = 0; i < this.V; ++i) {
-			if (!visited[i]) {
-				fillOrder(i, visited, s);
-			}
-		}
-		// System.out.println(s);
-		// stdout: [1, 2, 4, 3, 0]
+  public void printSCC() {
+    boolean[] visited = new boolean[this.V];
+    Stack<Integer> s = new Stack<>();
+    //
+    for (int i = 0; i < this.V; ++i) {
+      if (!visited[i]) {
+        fillOrder(i, visited, s);
+      }
+    }
+    // System.out.println(s);
+    // stdout: [1, 2, 4, 3, 0]
 
-		//  1 <-- 0 <--- 3
-		//  |   ^        ^
-		//  |  /         |
-		// \/ /          |
-		//  2            4
+    //  1 <-- 0 <--- 3
+    //  |   ^        ^
+    //  |  /         |
+    // \/ /          |
+    //  2            4
 
 
-		// The points of reversing graph is that a reversed SCC
-		// is still a SCC.
-		// http://www.geeksforgeeks.org/strongly-connected-components/
-		SCCGraph reversed = this.getTranspose();
-		visited = new boolean[this.V];
-		while (!s.isEmpty()) {
-			int v = s.pop();
+    // The points of reversing graph is that a reversed SCC
+    // is still a SCC.
+    // http://www.geeksforgeeks.org/strongly-connected-components/
+    SCCGraph reversed = this.getTranspose();
+    visited = new boolean[this.V];
+    while (!s.isEmpty()) {
+      int v = s.pop();
 
-			if (!visited[v]) {
-				reversed.dfs(v, visited);
-				// STDOUT SCC group by group.
-				System.out.println();
-			}
-		}
-	}
+      if (!visited[v]) {
+        reversed.dfs(v, visited);
+        // STDOUT SCC group by group.
+        System.out.println();
+      }
+    }
+  }
 
-	// Helper function to add a edge to graph.
-	public void addEdge(int v, int w) {
-		this.adj.get(v).add(w);
-	}
+  // Helper function to add a edge to graph.
+  public void addEdge(int v, int w) {
+    this.adj.get(v).add(w);
+  }
 
-	// Fills Stack with vertices (in increasing order of finishing times)
-	// The top element of stack has the maximum finishing time
-	public void fillOrder(int v, boolean[] visited, Stack<Integer> s) {
-		visited[v] = true;
+  // Fills Stack with vertices (in increasing order of finishing times)
+  // The top element of stack has the maximum finishing time
+  public void fillOrder(int v, boolean[] visited, Stack<Integer> s) {
+    visited[v] = true;
 
-		for (int i : this.adj.get(v)) {
-			if (!visited[i]) { // Go on with unvisited node.
-				fillOrder(i, visited, s);
-			}
-		}
-		// After recursion is over, push itself into the stack.
-		// Which mean first seen, last pushed.
-		s.add(v);
-	}
+    for (int i : this.adj.get(v)) {
+      if (!visited[i]) { // Go on with unvisited node.
+        fillOrder(i, visited, s);
+      }
+    }
+    // After recursion is over, push itself into the stack.
+    // Which mean first seen, last pushed.
+    s.add(v);
+  }
 
-	// Create the reversed version of cur Graph.
-	public SCCGraph getTranspose() {
-		SCCGraph transposed = new SCCGraph(this.V);
-		for (int i = 0; i < this.V; ++i) {
-		    ArrayList<Integer> arr = this.adj.get(i);
-			for (int j = 0; j < arr.size(); ++j) {
-				// In original graph node i point to node j,
-				// then in reversed graph node j points to i.
-				transposed.addEdge(arr.get(j), i);
-			}
-		}
+  // Create the reversed version of cur Graph.
+  public SCCGraph getTranspose() {
+    SCCGraph transposed = new SCCGraph(this.V);
+    for (int i = 0; i < this.V; ++i) {
+      List<Integer> arr = this.adj.get(i);
+      for (int j = 0; j < arr.size(); ++j) {
+        // In original graph node i point to node j,
+        // then in reversed graph node j points to i.
+        transposed.addEdge(arr.get(j), i);
+      }
+    }
 
-		return transposed;
-	}
+    return transposed;
+  }
 
-	// No-brainer dfs.
-	public void dfs(int v, boolean[] visited) {
-		visited[v] = true;
-		System.out.print("<" + v + "> ");
+  // No-brainer dfs.
+  public void dfs(int v, boolean[] visited) {
+    visited[v] = true;
+    System.out.print("<" + v + "> ");
 
-		for (int i : this.adj.get(v)) {
-			if (!visited[i]) {
-				dfs(i, visited);
-			}
-		}
-	}
+    for (int i : this.adj.get(v)) {
+      if (!visited[i]) {
+        dfs(i, visited);
+      }
+    }
+  }
 }
---
+++ graph/leetcode_Course_Schedule.py
@@ -0,0 +1,119 @@
+"""leetcode_Course_Schedule
+
+https://leetcode.com/problems/course-schedule/
+
+There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+Some courses may have prerequisites, for example to take course 0 you have to
+first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it
+possible for you to finish all courses?
+
+For example:
+
+2, [[1,0]]
+There are a total of 2 courses to take. To take course 1 you should have
+finished course 0. So it is possible.
+
+2, [[1,0],[0,1]]
+There are a total of 2 courses to take. To take course 1 you should have
+finished course 0, and to take course 0 you should also have finished course 1.
+So it is impossible.
+"""
+
+from collections import deque
+
+
+class Solution:
+
+    """Circle detection in directed graph.
+
+    Note that the graph might have multiple sub-graph.
+    """
+
+    def canFinish(self, numCourses, prerequisites):
+        """"""
+        # Build adj list as graph.
+        graph = {}
+
+        for edge in prerequisites:
+            node_to = edge[0]
+            node_from = edge[1]
+            graph.setdefault(node_from, []).append(node_to)
+
+        # Cycle dection in DAG.
+        visited = [False] * numCourses
+
+        return self.is_acyclic(graph, numCourses)
+
+    def is_acyclic(self, graph, n):
+        """Return whether a graph is acyclic.
+
+        I might be overkilling this interview question here(might not) but it
+        is a wonderful practice as in general graph topic, SCC and Toposort.
+
+        The general idea to use scc/toposort to detect cyclic graph is:
+        1. Get the toposort order of all nodes, including those in sub-graphs
+        2. Transpose the graph
+        3. Given the toposort order, if every node has no unchecked neighbors
+           it points to, then it's an acyclic graph.
+
+        :param graph: dict, adj list
+        :param graph: int, number of nodes
+        """
+        def toposort(i, visited, stack, graph):
+            """Toposort graph and push order in stack."""
+            visited[i] = True
+            for j in graph.get(i, []):
+                if not visited[j]:
+                    toposort(j, visited, stack, graph)
+            stack.append(i)
+
+        def transpose_graph(graph):
+            """Return the transposed copy of a given graph.
+
+            :parram graph: dict, adj list
+            :parram return: dict, transposed graph in adj list
+            """
+            transposed = {}
+            for from_node, to_nodes in graph.iteritems():
+                for to_node in to_nodes:
+                    transposed.setdefault(to_node, []).append(from_node)
+            return transposed
+
+        visited = [False] * n
+        stack = []
+
+        # Step 1.
+        for i in xrange(n):
+            if not visited[i]:
+                toposort(i, visited, stack, graph)
+
+        # Step 2.
+        transposed = transpose_graph(graph)
+        # Step 3.
+        visited = [False] * n
+        while stack:
+            node = stack.pop()
+            if not visited[node]:
+                # Check node has no unvisited neighbors.
+                to_nodes = transposed.get(node, [])
+                for to_node in to_nodes:
+                    if not visited[to_node]:
+                        return False
+                visited[node] = True
+        return True
+
+
+def main():
+    sol = Solution()
+    assert sol.canFinish(2, [[1, 0]])
+    assert sol.canFinish(3, [[2, 0], [2, 1]])
+
+    assert not sol.canFinish(2, [[1,0],[0,1]])
+    assert not sol.canFinish(100, [[6,27],[83,9],[10,95],[48,67],[5,71],[18,72],[7,10],[92,4],[68,84],[6,41],[82,41],[18,54],[0,2],[1,2],[8,65],[47,85],[39,51],[13,78],[77,50],[70,56],[5,61],[26,56],[18,19],[35,49],[79,53],[40,22],[8,19],[60,56],[48,50],[20,70],[35,12],[99,85],[12,75],[2,36],[36,22],[21,15],[98,1],[34,94],[25,41],[65,17],[1,56],[43,96],[74,57],[19,62],[62,78],[50,86],[46,22],[10,13],[47,18],[20,66],[83,66],[51,47],[23,66],[87,42],[25,81],[60,81],[25,93],[35,89],[65,92],[87,39],[12,43],[75,73],[28,96],[47,55],[18,11],[29,58],[78,61],[62,75],[60,77],[13,46],[97,92],[4,64],[91,47],[58,66],[72,74],[28,17],[29,98],[53,66],[37,5],[38,12],[44,98],[24,31],[68,23],[86,52],[79,49],[32,25],[90,18],[16,57],[60,74],[81,73],[26,10],[54,26],[57,58],[46,47],[66,54],[52,25],[62,91],[6,72],[81,72],[50,35],[59,87],[21,3],[4,92],[70,12],[48,4],[9,23],[52,55],[43,59],[49,26],[25,90],[52,0],[55,8],[7,23],[97,41],[0,40],[69,47],[73,68],[10,6],[47,9],[64,24],[95,93],[79,66],[77,21],[80,69],[85,5],[24,48],[74,31],[80,76],[81,27],[71,94],[47,82],[3,24],[66,61],[52,13],[18,38],[1,35],[32,78],[7,58],[26,58],[64,47],[60,6],[62,5],[5,22],[60,54],[49,40],[11,56],[19,85],[65,58],[88,44],[86,58]])
+
+
+if __name__ == '__main__':
+    main()
